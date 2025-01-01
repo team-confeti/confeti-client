13445b6c3e4d651c1859a304e657b98c8fd53fff
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-01-01 21:12:04 +0900

    init: 프로젝트 생성

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@confeti/design-system';
+
+function App() {
+  return (
+    <main>
+      <h1>Confeti</h1>
+      <Button>Click me</Button>
+    </main>
+  );
+}
+
+export default App;
