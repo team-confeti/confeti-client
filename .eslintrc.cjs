39862bc0fef29e8e556c8bad7f05260d54e2273b
gwagjiug <kwjo0228@naver.com>
2025-01-03 18:33:04 +0900

    chore: eslint.cjs 속성 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,9 @@
 module.exports = {
+  'linebreak-style': [
+    'error',
+    require('os').EOL === '\r\n' ? 'windows' : 'unix',
+  ],
+  'prettier/prettier': ['error', { endOfLine: 'auto' }],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
