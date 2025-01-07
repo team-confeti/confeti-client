34336d17b18899b2390dc5daa7310a94cf529dec
bongtta <kce0419@naver.com>
2025-01-07 21:26:50 +0900

    init: tanstack-query 초기세팅

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,13 +1,21 @@
 import { Button, ThemeProvider } from '@confeti/design-system';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from './shared/utils/query-client';
 
 function App() {
   return (
-    <ThemeProvider>
-      <main>
-        <h1>Confeti</h1>
-        <Button>Click me</Button>
-      </main>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <main>
+          <h1>Confeti</h1>
+          <Button>Click me</Button>
+        </main>
+      </ThemeProvider>
+      <div style={{ fontSize: '16px' }}>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </div>
+    </QueryClientProvider>
   );
 }
 
