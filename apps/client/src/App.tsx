0f246d82097fabbd3956962a454e7af061947eb9
Dahyun <124647898+daahyunk@users.noreply.github.com>
2025-01-08 18:35:07 +0900

    test: ci cd retest

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -3,7 +3,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from './shared/utils/query-client';
 
-// ci-cd test
+// ci-cd test 2
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
