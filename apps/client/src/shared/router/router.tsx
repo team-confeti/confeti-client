fb6b78e1daaaa4b0dd4ea53bb989d10203b1111c
gwagjiug <kwjo0228@naver.com>
2025-01-08 20:02:11 +0900

    fix: HomePage 컴포넌트 임포트 수정

--- apps/client/src/shared/router/router.tsx
+++ apps/client/src/shared/router/router.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
-import { HomePage } from './lazy';
 import { Route, Routes } from 'react-router-dom';
 import GlobalLayout from './global-layout';
+import { HomePage } from './lazy';
 import { routePath } from '@shared/constants/path';
 
 export default function Router() {
