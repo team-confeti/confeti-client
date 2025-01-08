15ddf67a13d33c1d712ac14fb44ebdc271b0ca6b
Dahyun <124647898+daahyunk@users.noreply.github.com>
2025-01-08 18:38:06 +0900

    chore: ci cd 테스트 주석 제거

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -3,7 +3,6 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from './shared/utils/query-client';
 
-// ci-cd test 2
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
