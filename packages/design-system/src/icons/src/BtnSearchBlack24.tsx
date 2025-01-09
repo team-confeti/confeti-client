c795782c554f590bb24e86d8a2da08ec3814df71
bongtta <kce0419@naver.com>
2025-01-10 01:10:00 +0900

    feat: svg 아이콘 컴포넌트로 변환

---
+++ packages/design-system/src/icons/src/BtnSearchBlack24.tsx
@@ -0,0 +1,17 @@
+import type { SVGProps } from 'react';
+const SvgBtnSearchBlack24 = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 25"
+    {...props}
+  >
+    <path
+      fill="#43444B"
+      d="M18.9 20.6 13.3 15q-.75.6-1.725.95T9.5 16.3q-2.725 0-4.612-1.887T3 9.8t1.888-4.613Q6.775 3.3 9.5 3.3t4.613 1.887Q16 7.075 16 9.8a6.1 6.1 0 0 1-1.3 3.8l5.625 5.625a.92.92 0 0 1 .275.675q0 .4-.3.7a.95.95 0 0 1-.7.275.95.95 0 0 1-.7-.275m-9.4-6.3q1.875 0 3.188-1.312Q14 11.675 14 9.8t-1.312-3.188Q11.375 5.3 9.5 5.3T6.312 6.612 5 9.8t1.312 3.188Q7.625 14.3 9.5 14.3"
+    />
+  </svg>
+);
+export default SvgBtnSearchBlack24;
---
+++ packages/design-system/src/icons/src/IcArrowGray16.tsx
@@ -0,0 +1,24 @@
+import type { SVGProps } from 'react';
+const SvgIcArrowGray16 = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 16 17"
+    {...props}
+  >
+    <g clipPath="url(#ic_arrow_gray_16_svg__a)">
+      <path
+        fill="#93959D"
+        d="M4.92 14.307a.833.833 0 0 0 1.18 0l5.54-5.54c.26-.26.26-.68 0-.94L6.1 2.287a.833.833 0 0 0-1.18 0 .833.833 0 0 0 0 1.18L9.746 8.3l-4.833 4.833a.83.83 0 0 0 .007 1.174"
+      />
+    </g>
+    <defs>
+      <clipPath id="ic_arrow_gray_16_svg__a">
+        <path fill="#fff" d="M0 .3h16v16H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default SvgIcArrowGray16;
