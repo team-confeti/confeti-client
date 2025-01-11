c57457cef585e3901bdb1625a29f9614eeef5c73
ce. <154000318+bongtta@users.noreply.github.com>
2025-01-11 23:59:20 +0900

    Feat: svg 아이콘 추가 변환 (#54)
    * feat: svg 아이콘 컴포넌트로 변환
    
    * feat: 추가 svg 아이콘 변환환
    
    * feat: BtnLink 아이콘 및 관련 파일 삭제
    
    ---------
    
    Co-authored-by: Minha Kang <118591632+m2na7@users.noreply.github.com>

---
+++ packages/design-system/src/icons/src/IcIndicatorDefault.tsx
@@ -0,0 +1,14 @@
+import type { SVGProps } from 'react';
+const SvgIcIndicatorDefault = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 6 6"
+    {...props}
+  >
+    <rect width={6} height={6} fill="#C5C6CB" rx={3} />
+  </svg>
+);
+export default SvgIcIndicatorDefault;
