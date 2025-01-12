bcdc28f0c6820daf450b5d56e790e7bd75ad88ea
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-01-13 03:15:32 +0900

    feat: App.tsx내 ToastContainer 추가

--- apps/client/src/pages/home/page/home.tsx
+++ apps/client/src/pages/home/page/home.tsx
@@ -1,10 +1,14 @@
-import { Button, Footer } from '@confeti/design-system';
+import { Button, Footer, toast } from '@confeti/design-system';
 
 const Home = () => {
+  const handleButtonClick = () => {
+    toast.default('두 글자 이상 입력해주세요');
+  };
+
   return (
     <>
       <h1>Home</h1>
-      <Button>Click Me</Button>
+      <Button onClick={handleButtonClick}>Click Me</Button>
       <Footer />
     </>
   );
