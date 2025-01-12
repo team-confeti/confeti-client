2bf11470d509952590fdf177e0a9b1b8e03e64a4
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-01-13 03:10:00 +0900

    feat: Toast 유틸리티 함수 추가 및 이벤트 관리 구현

---
+++ packages/design-system/src/components/toast/utils/toast.ts
@@ -0,0 +1,37 @@
+import { ToastEvent, ToastProps, ToastType } from '../types/type';
+import { eventManager } from './eventManager';
+
+type ToastAddFunctionProps = Omit<ToastProps, 'toastId' | 'type'>;
+type ToastOptions = Omit<ToastProps, 'toastId' | 'type' | 'text'>;
+const generateUniqueId = () => {
+  return Date.now().toString(36) + Math.random().toString(36).substring(4);
+};
+
+const emitAddToast = (type: ToastType, toastProps: ToastAddFunctionProps) => {
+  const id = generateUniqueId();
+  eventManager.emit(ToastEvent.Add, {
+    ...toastProps,
+    toastId: id,
+    type,
+  });
+  return id;
+};
+
+const emitUpdateToast = (toastId: string, text: string) => {
+  eventManager.emit(ToastEvent.Update, toastId, text);
+  return toastId;
+};
+
+const emitDeleteToast = (toastId: string) => {
+  eventManager.emit(ToastEvent.Delete, toastId);
+};
+
+export const toast = {
+  default: (text: string, toastOptions?: ToastOptions) =>
+    emitAddToast('default', { text: text, ...toastOptions }),
+  success: (text: string, toastOptions?: ToastOptions) =>
+    emitAddToast('success', { text: text, ...toastOptions }),
+
+  update: (toastId: string, text: string) => emitUpdateToast(toastId, text),
+  delete: (toastId: string) => emitDeleteToast(toastId),
+};
