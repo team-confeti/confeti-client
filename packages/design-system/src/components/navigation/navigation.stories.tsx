048d70348e24a6dcdec4d1b255b899021ee084db
hanseo <yds06206@gmail.com>
2025-01-13 08:37:02 +0900

    feat: navigation story 작성

--- packages/design-system/src/components/navigation/navigation.stories.tsx
+++ packages/design-system/src/components/navigation/navigation.stories.tsx
@@ -1,17 +1,29 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta } from '@storybook/react';
 import Navigation from './navigation';
 
 const meta: Meta<typeof Navigation> = {
-  title: 'Components/Navigation',
+  title: 'Common/Navigation',
   component: Navigation,
   parameters: {
     layout: 'centered',
   },
+
   tags: ['autodocs'],
   args: {},
 };
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
-export const Example: Story = {};
+export const Default = () => (
+  <Navigation.Root defaultActiveTab={0}>
+    <Navigation.List>
+      <Navigation.Item index={0}>{'홈'}</Navigation.Item>
+      <Navigation.Item index={1}>{'타임테이블'}</Navigation.Item>
+    </Navigation.List>
+    <Navigation.Panels>
+      {/* TODO: 추후 페이지 연결 */}
+      <Navigation.Panel>홈페이지</Navigation.Panel>
+      <Navigation.Panel>타임테이블</Navigation.Panel>
+    </Navigation.Panels>
+  </Navigation.Root>
+);
