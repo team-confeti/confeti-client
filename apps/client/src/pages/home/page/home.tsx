ec9c83013cbe1ec82d70278bd9ad970aeb18afdf
daahyunk <ekgusdl0408@naver.com>
2025-01-13 20:43:28 +0900

    style: 헤더 예시 삭제

--- apps/client/src/pages/home/page/home.tsx
+++ apps/client/src/pages/home/page/home.tsx
@@ -1,13 +1,8 @@
-import { Button, Footer, Header } from '@confeti/design-system';
+import { Button, Footer } from '@confeti/design-system';
 
 const Home = () => {
   return (
     <>
-      <Header
-        variant="detail"
-        title="마이페이지"
-        onBackClick={() => alert('뒤로가기 버튼 클릭')}
-      />
       <h1>Home</h1>
       <Button>Click Me</Button>
       <Footer />
