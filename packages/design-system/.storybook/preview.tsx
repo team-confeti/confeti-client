58a31aadee98b6630fd97be65de5a2205c0b0b3f
Hanseo Kim <108131226+seueooo@users.noreply.github.com>
2025-01-14 20:57:55 +0900

    Feat(design-system): Navigation 컴포넌트 구현 (#61)
    * feat: Navigation 컴포넌트 구현
    
    * feat: 탭 언더바 구현
    
    * feat: navigation 컴포넌트 최종 구현(슬라이드 애니메이션 제외)
    
    * feat: navigation story 작성
    
    * feat: decorators 추가
    
    * feat: 탭 바 슬라이드 애니메이션 구현 중..
    
    * feat: 탭 바 슬라이드 제거
    
    * feat: 삭제된 cn 유틸리티 함수 재생성
    
    * feat: 네비게이션 underBar 애니메이션 추가
    
    * feat: 스토리북 전체 레이아웃 설정
    
    * feat: flexColumn 토큰 추가
    
    * style: 불필요한 주석 제거
    
    * refactor: cn 함수 내보내기 방식 변경
    
    * fix: navigation height px에서 rem으로 변경
    
    * feat: TAB_MENU 상수 추가
    
    ---------
    
    Co-authored-by: Minha Kang <118591632+m2na7@users.noreply.github.com>

--- packages/design-system/.storybook/preview.tsx
+++ packages/design-system/.storybook/preview.tsx
@@ -19,7 +19,7 @@ const preview: Preview = {
   },
   decorators: [
     (Story) => (
-      <div className={themeClass}>
+      <div style={{ width: '375px' }} className={themeClass}>
         <Story />
       </div>
     ),
