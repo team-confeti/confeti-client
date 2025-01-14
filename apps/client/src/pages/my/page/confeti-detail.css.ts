72088fc99b0cf55a3b2356984031770cbe9d9f3e
bongtta <kce0419@naver.com>
2025-01-15 01:01:31 +0900

    style: css 코드 수정

--- apps/client/src/pages/my/page/confeti-detail.css.ts
+++ apps/client/src/pages/my/page/confeti-detail.css.ts
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css';
 
 export const container = style({
   display: 'grid',
-  gridTemplateColumns: '1fr 1fr 1fr',
+  gridTemplateColumns: 'repeat(3, 1fr)',
   padding: '2.4rem 2rem',
   gridColumnGap: '1.75rem',
   gridRowGap: '3rem',
