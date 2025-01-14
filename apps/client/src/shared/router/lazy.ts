2f1f820cda8c219d07e6d307e1342e35aeb5e991
ce. <154000318+bongtta@users.noreply.github.com>
2025-01-15 01:03:54 +0900

    Feat(client): 공연 더보기 페이지 퍼블리싱 (#79)
    * feat: my-confeti 라우팅 추가
    
    * feat: my-confeti 페이지 퍼블리싱
    
    * style: css 코드 수정

--- apps/client/src/shared/router/lazy.ts
+++ apps/client/src/shared/router/lazy.ts
@@ -2,3 +2,6 @@ import { lazy } from 'react';
 
 export const HomePage = lazy(() => import('@pages/home/page/home'));
 export const MyPage = lazy(() => import('@pages/my/page/my'));
+export const MyConfetiPage = lazy(
+  () => import('@pages/my/page/confeti-detail'),
+);
