71f2178bbc1dbfea5b95040f24c4bfae53a2fdb2
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-01-14 21:16:17 +0900

    feat: border디자인 토큰 lime3 추가

--- apps/client/src/pages/my/components/logout-button.css.ts
+++ apps/client/src/pages/my/components/logout-button.css.ts
@@ -7,7 +7,7 @@ export const button = style({
 
   padding: '0.7rem 1.25rem',
   borderRadius: '2rem',
-  border: `1px solid ${themeVars.color.confeti_lime3}`,
+  border: themeVars.border.lime3,
   gap: '0.2rem',
   color: themeVars.color.confeti_lime3,
   ...themeVars.fontStyles.subtitle5_sb_12,
