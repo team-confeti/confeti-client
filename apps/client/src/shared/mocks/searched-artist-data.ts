9aa5450d47471cd724fddf2d78aa8cf1e4c79a09
daahyunk <ekgusdl0408@naver.com>
2025-01-16 04:59:49 +0900

    chore: mock 데이터 수정

--- apps/client/src/shared/mocks/searched-artist-data.ts
+++ apps/client/src/shared/mocks/searched-artist-data.ts
@@ -6,6 +6,7 @@ export const searchedArtists = [
       'https://i.scdn.co/image/ab6761610000f17893db4a195c102e95153eb2e5',
     latestReleaseAt: '2024.09.11',
     isFavorite: true,
+    isMultipleArtists: false,
   },
   {
     artistId: 'daysix',
@@ -14,6 +15,7 @@ export const searchedArtists = [
       'https://i.namu.wiki/i/qEXLXPtXHaIupf9gAR0whqLMOvmiuDr65AUeB3hJ4oeXqoKuQFA5p7ToQcLv9sJTRw6-iR97Al08ESXvr-LXGQ.webp',
     latestReleaseAt: '2024.09.01',
     isFavorite: false,
+    isMultipleArtists: true,
   },
   {
     artistId: 'chae',
@@ -21,6 +23,7 @@ export const searchedArtists = [
     profileUrl:
       'https://i.namu.wiki/i/C_FQYTT11752GT86r-oGBolu1Isud9GLOaguU0ngfhRI69ySBkfbpeEv-8VisiWxf2A0Leeu8kyXQWPVxc9Rlw.webp',
     latestReleaseAt: '2024.09.01',
-    isFavorite: false,
+    isFavorite: true,
+    isMultipleArtists: false,
   },
 ];
--- apps/client/src/shared/mocks/searched-performance-data.ts
+++ apps/client/src/shared/mocks/searched-performance-data.ts
@@ -12,17 +12,6 @@ export const searchedPerformances = [
   },
   {
     performanceId: 2,
-    artistId: 'aimyon',
-    type: 'festival',
-    title: '아이묭 콘서트',
-    performanceAt: '2025.08.01 - 2025.08.03',
-    posterUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6-CDA4DPG8NP6uE3bcCXcpHuER2qoiz15BQ&s',
-    area: '송도달빛축제공원',
-    isFavorite: false,
-  },
-  {
-    performanceId: 3,
     artistId: 'chae',
     type: 'festival',
     title: '24살이 된 김채은의 락락락 페스티벌',
@@ -30,14 +19,14 @@ export const searchedPerformances = [
     posterUrl:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6-CDA4DPG8NP6uE3bcCXcpHuER2qoiz15BQ&s',
     area: '고척돔',
-    isFavorite: false,
+    isFavorite: true,
   },
   {
-    performanceId: 4,
+    performanceId: 3,
     artistId: 'daysix',
     type: 'festival',
     title: '인천 펜타포트 락 페스티벌 2025',
-    performanceAt: '2025.08.01 - 2025.08.03',
+    performanceAt: '2025.01.18',
     posterUrl:
       'https://www.wevity.com/upload/contest/20240529022004_054149f7.jpg',
     area: '송도달빛축제공원',
