2ec43798e9a8b137000af6aca43cc6239cf13e59
gwagjiug <kwjo0228@naver.com>
2025-01-15 21:35:13 +0900

    Feat: 무한 캐러셀 컴포넌트를 위한 배너 데이터 추가

---
+++ packages/design-system/src/components/infinite-carousel/mocks/bottom-banner-data.ts
@@ -0,0 +1,34 @@
+export const BANNER_DATA = {
+  data: {
+    performanceCount: 3,
+    performances: [
+      {
+        index: 1,
+        performanceId: 1,
+        type: 'concert',
+        subtitle: '실리카겔 공연',
+        reserveAt: '2025.10.21',
+        reservationBgUrl:
+          'https://koreanmusicawards.com/wp-content/uploads/2023/02/2023_SilicaGel.jpeg',
+      },
+      {
+        index: 2,
+        performanceId: 2,
+        type: 'festival',
+        subtitle: '고고학 공연',
+        reserveAt: '2025.10.21',
+        reservationBgUrl:
+          'https://www.artinsight.co.kr/data/tmp/2409/20240908195947_tcowsphc.jpg',
+      },
+      {
+        index: 3,
+        performanceId: 3,
+        type: 'concert',
+        subtitle: '오아시스 내한공연',
+        reserveAt: '2025.10.21',
+        reservationBgUrl:
+          'https://i.namu.wiki/i/pwCmFe3bH3uWGNvInRsUMNgMOANn9OaQ-ARvuxzsr3ZyJjSGrLoAtwkWI0xkoeDlum_DY3rXLb-OlfI7BM_h2w.webp',
+      },
+    ],
+  },
+};
