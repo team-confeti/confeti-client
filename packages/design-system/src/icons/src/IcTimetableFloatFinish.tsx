fc037f57ea752710058e8bfe7e868f2154cc13f4
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-01-16 02:40:57 +0900

    feat: TimeTable 관련 아이콘 추가

---
+++ packages/design-system/src/icons/src/IcTimetableFloatFinish.tsx
@@ -0,0 +1,24 @@
+import type { SVGProps } from 'react';
+const SvgIcTimetableFloatFinish = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <g clipPath="url(#ic_timetable_float_finish_svg__a)">
+      <path
+        fill="#E9FFAC"
+        d="M12 4c-4.416 0-8 3.584-8 8s3.584 8 8 8 8-3.584 8-8-3.584-8-8-8m0 14.4A6.41 6.41 0 0 1 5.6 12c0-3.528 2.872-6.4 6.4-6.4s6.4 2.872 6.4 6.4-2.872 6.4-6.4 6.4m3.104-9.368L10.4 13.736l-1.504-1.504a.797.797 0 1 0-1.128 1.128l2.072 2.072a.797.797 0 0 0 1.128 0l5.272-5.272a.797.797 0 0 0 0-1.128.806.806 0 0 0-1.136 0"
+      />
+    </g>
+    <defs>
+      <clipPath id="ic_timetable_float_finish_svg__a">
+        <path fill="#fff" d="M0 0h24v24H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default SvgIcTimetableFloatFinish;
