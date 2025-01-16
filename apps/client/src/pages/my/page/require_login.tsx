483346bf3ebe9412857d33df2b40dde570f02b7c
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-01-17 01:30:01 +0900

    feat: 로그인 필요 안내 페이지 컴포넌트 추가

---
+++ apps/client/src/pages/my/page/require_login.tsx
@@ -0,0 +1,13 @@
+import { Header } from '@confeti/design-system';
+import RequireLoginSection from '../components/auth/require_login_section';
+
+const RequireLogin = () => {
+  return (
+    <div>
+      <Header variant="detail" title="마이페이지" />
+      <RequireLoginSection />
+    </div>
+  );
+};
+
+export default RequireLogin;
