925f9b365157549e5995bd80b87371904368f886
Hanseo Kim <108131226+seueooo@users.noreply.github.com>
2025-01-16 14:37:30 +0900

    Feat(design-system): 홈 배너 상단 캐러셀 구현 (#80)
    * feat: TopCarousel 컴포넌트 구현
    
    * feat: 캐러셀 구현
    
    * feat: 홈배너 상단 캐러셀 구현
    
    * feat: story 작성
    
    * design: 스타일 추가
    
    * feat: 탭 변경 시 패널 연결
    
    * refactor: 필요없는 코드 삭제
    
    * design: style 수정
    
    * fix: 애니메이션 오류 해결
    
    * fix: 타입 오류 제거
    
    * feat: css 파일 vanila-extract로 변환
    
    * chore: 목데이터 이미지 수정
    
    * refactor: 코드리뷰 반영
    
    * design: 스타일 theme 으로 변경
    
    * refactor: 코드리뷰 반영

---
+++ packages/design-system/src/components/top-carousel/dots.css.ts
@@ -0,0 +1,37 @@
+import { globalStyle } from '@vanilla-extract/css';
+import { themeVars } from '../../styles';
+
+globalStyle('.dots_custom', {
+  display: 'flex',
+  justifyContent: 'center',
+  verticalAlign: 'middle',
+  margin: 0,
+  paddingTop: '0rem',
+  paddingBottom: '1.6rem',
+});
+
+globalStyle('.dots_custom li', {
+  listStyle: 'none',
+  cursor: 'pointer',
+  display: 'inline-block',
+  marginRight: '0.8rem',
+  padding: 0,
+});
+
+globalStyle('.dots_custom li button', {
+  border: 'none',
+  background: themeVars.color.white,
+  opacity: 0.2,
+  cursor: 'pointer',
+  display: 'block',
+  height: '0.6rem',
+  width: '0.6rem',
+  borderRadius: '100%',
+  padding: 0,
+  color: 'transparent',
+});
+
+globalStyle('.dots_custom li.slick-active button', {
+  backgroundColor: themeVars.color.confeti_lime3,
+  opacity: 1,
+});
