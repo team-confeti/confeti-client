2bd724d9aed71f782ecca045e6752d8634337cb3
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-01-17 01:32:36 +0900

    feat: 로그아웃 기능 구현

--- apps/client/src/pages/my/components/logout-button.tsx
+++ apps/client/src/pages/my/components/logout-button.tsx
@@ -3,11 +3,12 @@ import * as styles from './logout-button.css.ts';
 
 interface Props {
   text: string;
+  onClick: () => void;
 }
 
-const LogoutButton = ({ text }: Props) => {
+const LogoutButton = ({ text, onClick }: Props) => {
   return (
-    <button className={styles.button}>
+    <button className={styles.button} onClick={onClick}>
       {text}
       <IcArrowGray16 />
     </button>
