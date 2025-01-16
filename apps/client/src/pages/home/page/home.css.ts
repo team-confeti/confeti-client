66e28fbb2342f0e66bed0938f139303c2b5e062d
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-01-17 03:07:53 +0900

    Fix(client): 글로벌 스타일 수정 (#104)
    * fix: 반응형 구현중
    
    * fix: 상단 캐러셀 화살표 비활성화
    
    * refactor: globalStyle 수정
    
    ---------
    
    Co-authored-by: hanseo <yds06206@gmail.com>

--- apps/client/src/pages/home/page/home.css.ts
+++ apps/client/src/pages/home/page/home.css.ts
@@ -2,6 +2,7 @@ import { style } from '@vanilla-extract/css';
 import { themeVars } from '@confeti/design-system/styles';
 
 export const mainStyle = style({
+  width: '100%',
   paddingTop: '3rem',
   background: themeVars.color.confeti_purple_grad,
 });
