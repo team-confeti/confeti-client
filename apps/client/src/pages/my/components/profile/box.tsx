7b368db176943d9584f673de7108732878050134
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-01-17 03:07:40 +0900

    Feat(client): 마이페이지 추가 (#103)
    * feat: 로그인 필요 안내 페이지 컴포넌트 추가
    
    * feat: 마이 페이지 중첩 라우팅 구현
    
    * feat: 로그아웃 기능 구현
    
    * feat: 더보기 버튼 클릭시 페이지 경로 이동 기능 구현
    
    * feat: my-page 폴더구조 재설정
    
    * feat: 라우터 네이밍 수정 및 주석 설정

--- apps/client/src/pages/my/components/profile/box.tsx
+++ apps/client/src/pages/my/components/profile/box.tsx
@@ -1,20 +1,30 @@
+import { useNavigate } from 'react-router-dom';
 import { IcArrowGray16 } from '@confeti/design-system/icons';
 import * as styles from './box.css';
 
 interface BoxProps {
   title: string;
-  children: React.ReactNode;
+  path: string;
   showMore?: boolean;
+  children: React.ReactNode;
 }
 
-const Box = ({ title, children, showMore = false }: BoxProps) => {
+const Box = ({ title, path, showMore = false, children }: BoxProps) => {
+  const navigate = useNavigate();
+
+  const handleShowMore = () => {
+    navigate(path);
+  };
+
   return (
     <section className={styles.container}>
       <div className={styles.header}>
         <h3 className={styles.title}>{title}</h3>
         {showMore && (
           <div className={styles.buttonWrapper}>
-            <button className={styles.button}>더보기</button>
+            <button onClick={handleShowMore} className={styles.button}>
+              더보기
+            </button>
             <IcArrowGray16 className={styles.icon} />
           </div>
         )}
--- apps/client/src/shared/constants/path.ts
+++ apps/client/src/shared/constants/path.ts
@@ -1,9 +1,14 @@
 export const routePath = {
+  // home-page
   ROOT: '/',
+  // my-page
   MY: '/my',
+  MY_ARTIST: 'artist',
+  MY_CONFETI: 'confeti',
+  MY_REQUIRE_LOGIN: 'require-login',
+  // search-page
   SEARCH: '/search',
-  MYCONFETI: '/my-confeti',
+  // detail-page
   CONCERT: '/concert-detail/:concertId',
   FESTIVAL: '/festival-detail/:festivalId',
-  MYARTIST: '/my-artist',
 } as const;
