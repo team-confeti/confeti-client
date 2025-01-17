144b6b4ea38a9f79f4e65848d79c50c3c106b56f
daahyunk <ekgusdl0408@naver.com>
2025-01-18 07:06:49 +0900

    feat: box shadow 토큰 추가

--- packages/design-system/src/styles/tokens/box-shadow.ts
+++ packages/design-system/src/styles/tokens/box-shadow.ts
@@ -5,4 +5,7 @@ export const shadowStyles = {
   shadow_md_2: {
     boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
   },
+  shadow_md_3: {
+    boxShadow: '0 -4px 5px -1px rgba(0, 0, 0, 0.2)',
+  },
 };
