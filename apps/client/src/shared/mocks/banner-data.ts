e281594dce1eedad9f54dda4bdbfcaf794efeafd
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-01-18 02:35:59 +0900

    refactor: 배너 이미지 더미데이터 추가

--- apps/client/src/shared/mocks/banner-data.ts
+++ apps/client/src/shared/mocks/banner-data.ts
@@ -5,7 +5,8 @@ export const PERFORMANCE_DATA = [
     title: '오아시스 내한공연1',
     subTitle: 'LIVE NATION PRESENTS COLDPLAY',
     performanceAt: '2025.10.21',
-    posterUrl: 'https://dummyimage.com/156x208',
+    posterUrl:
+      'https://s3-alpha-sig.figma.com/img/cd74/1748/2c7fa3513d56a30582a1b24ea7ceb408?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=X0l2RAVxVnOjB6RIM81l-5cVWBY8w4Dm~uMXKQIybjN3I9q5i8rVEgI698FBwi0PG8~LTtqiON6wF6-LldHtV~syc~dOzMHTYWp~tN4s1qyoiHARHiR78SxWYHA3~h1ZSQloVvCUx0Kq4mCQ9yBBANwE1y5SLnx8P9IQpAIE48iyrk35sKVlF80rlfDaFAXTJXv940YoT7GOFqnYdkd5ep1bDQpj2wNmTNnl54jlV8k450yvvn~f5f9hV7YN62AgRAJ6YI-62UpSGBKIXoNXHRja2nl0RZngtgoXMccv33kvZ3KPE8iH~boEeolwVt6FL-lKlFqG8G5O4mZS994yZQ__ ',
   },
   {
     performanceId: 2,
@@ -13,7 +14,8 @@ export const PERFORMANCE_DATA = [
     title: '오아시스 내한공연2',
     subTitle: '',
     performanceAt: '2025.10.21',
-    posterUrl: 'https://dummyimage.com/156x208',
+    posterUrl:
+      'https://s3-alpha-sig.figma.com/img/f03d/9a2c/4854283ea449b35300d949bc3d7e3bcb?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bJBmX7kA6jAfx1I5CLhFl763ycOSD6eL3Gkg~Immdm8cGWDjSmOcW0VU1X23~q-wgDp0uTya6O9oMTy6dpHlkedb8RbZnTHMNlhVXbbNU5-TVwVJSTpIkCj8BkC8ble~jKhESsWt6OZq3f2uA5j4ESb-VaYMuq7iK-LlBTXcgKFfyZC5o7z2Y-7uASgM6-uWitKCsUUUXON4tft1PMI4~asveZyc4k2-ZGCyd0dwpNONefXkwXMwgYjmoOK12uzB7dKoSrGJPcWEDMYsdZHbG8x5rLsBVe87U~eTmfN5s-U5dDu0Sv0~u8s~W4vddQh0CaZSlqSViO5I373KI7pHOQ__',
   },
   {
     performanceId: 3,
@@ -21,7 +23,8 @@ export const PERFORMANCE_DATA = [
     title: '오아시스 내한공연3',
     subTitle: 'LIVE NATION PRESENTS COLDPLAY',
     performanceAt: '2025.10.21',
-    posterUrl: 'https://dummyimage.com/156x208',
+    posterUrl:
+      'https://s3-alpha-sig.figma.com/img/cd74/1748/2c7fa3513d56a30582a1b24ea7ceb408?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=X0l2RAVxVnOjB6RIM81l-5cVWBY8w4Dm~uMXKQIybjN3I9q5i8rVEgI698FBwi0PG8~LTtqiON6wF6-LldHtV~syc~dOzMHTYWp~tN4s1qyoiHARHiR78SxWYHA3~h1ZSQloVvCUx0Kq4mCQ9yBBANwE1y5SLnx8P9IQpAIE48iyrk35sKVlF80rlfDaFAXTJXv940YoT7GOFqnYdkd5ep1bDQpj2wNmTNnl54jlV8k450yvvn~f5f9hV7YN62AgRAJ6YI-62UpSGBKIXoNXHRja2nl0RZngtgoXMccv33kvZ3KPE8iH~boEeolwVt6FL-lKlFqG8G5O4mZS994yZQ__',
   },
   {
     performanceId: 4,
@@ -29,7 +32,8 @@ export const PERFORMANCE_DATA = [
     title: '오아시스 내한공연4',
     subTitle: '',
     performanceAt: '2025.10.21',
-    posterUrl: 'https://dummyimage.com/156x208',
+    posterUrl:
+      'https://s3-alpha-sig.figma.com/img/46b2/a7e8/7992b9ede09843fff6ebf8b566713967?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=lyWMYVAsud4Do0xSlFj4Ex59sruTIo9ZQhQLPbLtxW9W5wOn0ppLfC81tFHXWgiyXKK4OiHc79CouyY0jFdN1AIrsGM3eKdJDjEh8yU0zQxH5Pyp5aOqX4uaiWqMP17Cyj7xaLrKJgDWPAwJAOsonzns~ihB67MrZKxdEqKo0F5nM-nYJwgYLQSlL5kmQwAm3ig2QUvSRNHhDWw2XPMD6Ng9JktfeWv4npBoRRG8WpboXSt2O72cVoNZbxPFuh1fBY5jRC4oNbucMiHRKCWRst-bRrDNKI-gS8segXXyGAXOyPTIls0SZ~bjOzFfSDmVzvmOvQ-Q4~xh502bV1tziQ__',
   },
   {
     performanceId: 5,
@@ -37,7 +41,8 @@ export const PERFORMANCE_DATA = [
     title: '오아시스 내한공연5',
     subTitle: 'LIVE NATION PRESENTS COLDPLAY',
     performanceAt: '2025.10.21',
-    posterUrl: 'https://dummyimage.com/156x208',
+    posterUrl:
+      'https://s3-alpha-sig.figma.com/img/f03d/9a2c/4854283ea449b35300d949bc3d7e3bcb?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bJBmX7kA6jAfx1I5CLhFl763ycOSD6eL3Gkg~Immdm8cGWDjSmOcW0VU1X23~q-wgDp0uTya6O9oMTy6dpHlkedb8RbZnTHMNlhVXbbNU5-TVwVJSTpIkCj8BkC8ble~jKhESsWt6OZq3f2uA5j4ESb-VaYMuq7iK-LlBTXcgKFfyZC5o7z2Y-7uASgM6-uWitKCsUUUXON4tft1PMI4~asveZyc4k2-ZGCyd0dwpNONefXkwXMwgYjmoOK12uzB7dKoSrGJPcWEDMYsdZHbG8x5rLsBVe87U~eTmfN5s-U5dDu0Sv0~u8s~W4vddQh0CaZSlqSViO5I373KI7pHOQ__',
   },
   {
     performanceId: 6,
@@ -45,7 +50,8 @@ export const PERFORMANCE_DATA = [
     title: '오아시스 내한공연6',
     subTitle: 'LIVE NATION PRESENTS COLDPLAY',
     performanceAt: '2025.10.21',
-    posterUrl: 'https://dummyimage.com/156x208',
+    posterUrl:
+      'https://s3-alpha-sig.figma.com/img/cd74/1748/2c7fa3513d56a30582a1b24ea7ceb408?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=X0l2RAVxVnOjB6RIM81l-5cVWBY8w4Dm~uMXKQIybjN3I9q5i8rVEgI698FBwi0PG8~LTtqiON6wF6-LldHtV~syc~dOzMHTYWp~tN4s1qyoiHARHiR78SxWYHA3~h1ZSQloVvCUx0Kq4mCQ9yBBANwE1y5SLnx8P9IQpAIE48iyrk35sKVlF80rlfDaFAXTJXv940YoT7GOFqnYdkd5ep1bDQpj2wNmTNnl54jlV8k450yvvn~f5f9hV7YN62AgRAJ6YI-62UpSGBKIXoNXHRja2nl0RZngtgoXMccv33kvZ3KPE8iH~boEeolwVt6FL-lKlFqG8G5O4mZS994yZQ__',
   },
   {
     performanceId: 7,
@@ -53,7 +59,8 @@ export const PERFORMANCE_DATA = [
     title: '오아시스 내한공연7',
     subTitle: '',
     performanceAt: '2025.10.21',
-    posterUrl: 'https://dummyimage.com/156x208',
+    posterUrl:
+      'https://s3-alpha-sig.figma.com/img/f03d/9a2c/4854283ea449b35300d949bc3d7e3bcb?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bJBmX7kA6jAfx1I5CLhFl763ycOSD6eL3Gkg~Immdm8cGWDjSmOcW0VU1X23~q-wgDp0uTya6O9oMTy6dpHlkedb8RbZnTHMNlhVXbbNU5-TVwVJSTpIkCj8BkC8ble~jKhESsWt6OZq3f2uA5j4ESb-VaYMuq7iK-LlBTXcgKFfyZC5o7z2Y-7uASgM6-uWitKCsUUUXON4tft1PMI4~asveZyc4k2-ZGCyd0dwpNONefXkwXMwgYjmoOK12uzB7dKoSrGJPcWEDMYsdZHbG8x5rLsBVe87U~eTmfN5s-U5dDu0Sv0~u8s~W4vddQh0CaZSlqSViO5I373KI7pHOQ__',
   },
 ];
 
