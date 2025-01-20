c472f7c91c4e0872f069fd0c89c9f68e9c5ace1d
hanseo <yds06206@gmail.com>
2025-01-21 01:52:49 +0900

    feat: 이미지 저장 버튼 추가

--- apps/client/src/pages/time-table/components/time-table-section/time-table-section.css.ts
+++ apps/client/src/pages/time-table/components/time-table-section/time-table-section.css.ts
@@ -1,5 +1,13 @@
 import { style } from '@vanilla-extract/css';
 
 export const container = style({
-  padding: '4.2rem 2rem',
+  padding: '4.2rem 0rem 2rem',
+});
+
+export const bottomWrapper = style({
+  padding: '2rem 2rem 0rem',
+});
+
+export const saveButton = style({
+  borderRadius: '5px',
 });
--- apps/client/src/pages/time-table/components/time-table-section/time-table-section.tsx
+++ apps/client/src/pages/time-table/components/time-table-section/time-table-section.tsx
@@ -2,13 +2,22 @@ import { TIME_TABLE_INFO } from '@shared/mocks/time-table';
 import TimeTableBoard from '@pages/time-table/components/time-table-board/time-table-board';
 import Stage from '@pages/time-table/components/stage/stage';
 import * as styles from './time-table-section.css';
-
+import { Button } from '@confeti/design-system';
 const TimeTableSection = () => {
   return (
-    <section className={styles.container}>
-      <Stage timeTableInfo={TIME_TABLE_INFO}></Stage>
-      <TimeTableBoard timeTableInfo={TIME_TABLE_INFO}></TimeTableBoard>
-    </section>
+    <>
+      <section className={styles.container}>
+        <Stage timeTableInfo={TIME_TABLE_INFO}></Stage>
+        <TimeTableBoard timeTableInfo={TIME_TABLE_INFO}></TimeTableBoard>
+        <div className={styles.bottomWrapper}>
+          <Button
+            text="이미지 저장"
+            variant="add"
+            className={styles.saveButton}
+          ></Button>
+        </div>
+      </section>
+    </>
   );
 };
 
