3fae0f3bf418a10e2092426305aabdea34560527
gwagjiug <kwjo0228@naver.com>
2025-01-21 03:24:54 +0900

    feat: API 에러 처리 및 토큰 설정을 위한 인터셉터 추가

---
+++ apps/client/src/shared/apis/interceptor.ts
@@ -0,0 +1,32 @@
+import { HTTP_STATUS_CODE } from '@shared/constants/api';
+import { AxiosError, InternalAxiosRequestConfig } from 'axios';
+import { HTTPError } from './http-error';
+import { USER_ID_KEY } from '@shared/constants/user-constants';
+
+interface ErrorResponse {
+  success?: boolean;
+  message?: string;
+  code?: number;
+}
+
+export const handleAPIError = (error: AxiosError<ErrorResponse>) => {
+  if (!error.response) throw error;
+
+  const { data, status } = error.response;
+
+  if (status >= HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR) {
+    throw new HTTPError(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR, data.message);
+  }
+
+  throw new HTTPError(status, data.message);
+};
+
+export const handleCheckAndSetToken = (config: InternalAxiosRequestConfig) => {
+  const token = localStorage.getItem(USER_ID_KEY);
+
+  if (token) {
+    config.headers.Authorization = token;
+  }
+
+  return config;
+};
