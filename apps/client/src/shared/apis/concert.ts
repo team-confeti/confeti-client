1780bf42e424c24d6761390d99e5e4d31f536fda
daahyunk <ekgusdl0408@naver.com>
2025-01-21 05:07:42 +0900

    feat: 인스턴스 수정 사항 반영

--- apps/client/src/shared/apis/concert.ts
+++ apps/client/src/shared/apis/concert.ts
@@ -1,12 +1,12 @@
 import { BaseResponse } from '@shared/types/api';
 import { ConcertDetailResponse } from '@shared/types/concert-response';
 import { END_POINT } from '@shared/constants/api';
-import { instance } from './api';
+import { axiosInstance } from './config/instance';
 
 export const getConcertDetail = async (
   concertId: number,
 ): Promise<ConcertDetailResponse> => {
-  const response = await instance.get<BaseResponse<ConcertDetailResponse>>(
+  const response = await axiosInstance.get<BaseResponse<ConcertDetailResponse>>(
     `${END_POINT.GET_CONCERT_DETAIL}/${concertId}`,
   );
   return response.data.data;
--- apps/client/src/shared/apis/festival.ts
+++ apps/client/src/shared/apis/festival.ts
@@ -1,13 +1,13 @@
 import { BaseResponse } from '@shared/types/api';
 import { FestivalDetailResponse } from '@shared/types/festival-response';
 import { END_POINT } from '@shared/constants/api';
-import { instance } from './api';
+import { axiosInstance } from './config/instance';
 
 export const getFestivalDetail = async (
   festivalId: number,
 ): Promise<FestivalDetailResponse> => {
-  const response = await instance.get<BaseResponse<FestivalDetailResponse>>(
-    `${END_POINT.GET_FESTIVAL_DETAIL}/${festivalId}`,
-  );
+  const response = await axiosInstance.get<
+    BaseResponse<FestivalDetailResponse>
+  >(`${END_POINT.GET_FESTIVAL_DETAIL}/${festivalId}`);
   return response.data.data;
 };
