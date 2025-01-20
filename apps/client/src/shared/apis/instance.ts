32616baff8a6b9edc817d804eaf033649e3a1f31
gwagjiug <kwjo0228@naver.com>
2025-01-21 03:24:18 +0900

    feat: Axios 인스턴스 및 HTTP 메서드 유틸 함수 추가

---
+++ apps/client/src/shared/apis/instance.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { handleAPIError, handleCheckAndSetToken } from './interceptor';
+
+// Axios 인스턴스 생성
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+});
+
+export function get<T>(...args: Parameters<typeof axiosInstance.get>) {
+  return axiosInstance.get<T>(...args).then((res) => res.data);
+}
+
+export function post<T>(...args: Parameters<typeof axiosInstance.post>) {
+  return axiosInstance.post<T>(...args).then((res) => res.data);
+}
+
+export function put<T>(...args: Parameters<typeof axiosInstance.put>) {
+  return axiosInstance.put<T>(...args).then((res) => res.data);
+}
+
+export function patch<T>(...args: Parameters<typeof axiosInstance.patch>) {
+  return axiosInstance.patch<T>(...args).then((res) => res.data);
+}
+
+export function del<T>(...args: Parameters<typeof axiosInstance.delete>) {
+  return axiosInstance.delete<T>(...args).then((res) => res.data);
+}
+
+axiosInstance.interceptors.request.use(handleCheckAndSetToken);
+
+axiosInstance.interceptors.response.use((res) => res, handleAPIError);
