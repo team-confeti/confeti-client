031fc69d379a8123b45674879a03bc05a0827adf
gwagjiug <kwjo0228@naver.com>
2025-01-21 03:52:20 +0900

    feat: HTTPError 클래스에서 불필요한 공백 제거

--- apps/client/src/shared/apis/config/http-error.ts
+++ apps/client/src/shared/apis/config/http-error.ts
@@ -3,7 +3,6 @@ import { HTTP_STATUS_CODE } from '@shared/constants/api';
 /** HTTP 통신 시 발생하는 API Error를 별도의 객체로 나타내기 위한 에러 객체입니다. */
 export class HTTPError extends Error {
   statusCode: number;
-
   code?: number;
 
   constructor(statusCode: number, message?: string, code?: number) {
