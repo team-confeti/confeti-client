54a6090166443777f1bb1afc47cfd319b25f6afd
daahyunk <ekgusdl0408@naver.com>
2025-01-21 01:36:17 +0900

    refactor: suspense 글로벌 레이아웃 안으로 수정

--- apps/client/src/shared/router/router.tsx
+++ apps/client/src/shared/router/router.tsx
@@ -1,7 +1,5 @@
-import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import GlobalLayout from './global-layout';
-import Loading from '@shared/pages/loading/loading';
 import {
   ConcertDetailPage,
   FestivalDetailPage,
@@ -22,48 +20,42 @@ import { createProtectedRoute } from './protected-route';
 
 export default function Router() {
   return (
-    <Suspense fallback={<Loading />}>
-      <Routes>
-        <Route element={<GlobalLayout />}>
-          <Route path={routePath.ROOT} element={<HomePage />} />
+    <Routes>
+      <Route element={<GlobalLayout />}>
+        <Route path={routePath.ROOT} element={<HomePage />} />
 
-          <Route path={routePath.MY} element={<MyPage />}>
-            <Route
-              path=""
-              element={createProtectedRoute(true, <MyProfilePage />)}
-            />
-            <Route
-              path={routePath.MY_REQUIRE_LOGIN}
-              element={<RequireLoginPage />}
-            />
-            <Route
-              path={routePath.MY_ARTIST}
-              element={createProtectedRoute(true, <MyArtistPage />)}
-            />
-            <Route
-              path={routePath.MY_CONFETI}
-              element={createProtectedRoute(true, <MyConfetiPage />)}
-            />
-          </Route>
-
-          <Route path={routePath.SEARCH} element={<SearchPage />} />
-          <Route path={routePath.CONCERT} element={<ConcertDetailPage />} />
-          <Route path={routePath.FESTIVAL} element={<FestivalDetailPage />} />
-          <Route path={routePath.LOADING} element={<Loading />} />
-
+        <Route path={routePath.MY} element={<MyPage />}>
           <Route
-            path={routePath.TIME_TABLE_OUTLET}
-            element={<TimeTableLayout />}
-          >
-            <Route path="" element={<TimeTable />} />
-            <Route
-              path={routePath.TIME_TABLE_EMPTY_FESTIVAL}
-              element={<EmptyFestivalPage />}
-            />
-            <Route path={routePath.ADDFESTIVAL} element={<AddFestivalPage />} />
-          </Route>
+            path=""
+            element={createProtectedRoute(true, <MyProfilePage />)}
+          />
+          <Route
+            path={routePath.MY_REQUIRE_LOGIN}
+            element={<RequireLoginPage />}
+          />
+          <Route
+            path={routePath.MY_ARTIST}
+            element={createProtectedRoute(true, <MyArtistPage />)}
+          />
+          <Route
+            path={routePath.MY_CONFETI}
+            element={createProtectedRoute(true, <MyConfetiPage />)}
+          />
         </Route>
-      </Routes>
-    </Suspense>
+
+        <Route path={routePath.SEARCH} element={<SearchPage />} />
+        <Route path={routePath.CONCERT} element={<ConcertDetailPage />} />
+        <Route path={routePath.FESTIVAL} element={<FestivalDetailPage />} />
+
+        <Route path={routePath.TIME_TABLE_OUTLET} element={<TimeTableLayout />}>
+          <Route path="" element={<TimeTable />} />
+          <Route
+            path={routePath.TIME_TABLE_EMPTY_FESTIVAL}
+            element={<EmptyFestivalPage />}
+          />
+          <Route path={routePath.ADDFESTIVAL} element={<AddFestivalPage />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
