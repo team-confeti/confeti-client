184db07eb0c862c3ef6d08e18b6154b6270d1da9
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-01-22 05:00:29 +0900

    Feat(client): 타임테이블 EditFloatingButton 기능 구현 (#164)
    * feat: edit-floating-button 편집 모드 관리 훅 추가
    
    * feat: 스크롤 위치 감지 훅 추가
    
    * feat: 편집 모드에 따른 플로팅 버튼 위치 조정 및 상태 관리 개선
    
    * feat: 스크롤 하단 감지 기준을 50px에서 100px로 변경
    
    * feat: editFloatingButton 스크롤 관련 기능 추가
    
    * refactor: useButtonSelection 및 useEditModes export 형식 수정
    
    * feat: 타임테이블 캘린더 데이터가 없을 경우 설정

--- apps/client/src/pages/time-table/components/edit/edit-floating-button.tsx
+++ apps/client/src/pages/time-table/components/edit/edit-floating-button.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import {
   IcTimetableFloatClose,
   IcFloatEditLime24,
@@ -7,37 +5,43 @@ import {
   IcFloatDelete24,
   IcTimetableFloatFinish,
 } from '@confeti/design-system/icons';
-import * as styles from './edit-floating-button.css';
+import useDisableScroll from '@pages/time-table/hooks/use-disabled-scroll';
+import { useScrollPosition } from '@pages/time-table/hooks/use-scroll-position';
 import { EDIT_BOX, EDIT_BUTTON } from '../../constants/edit-floating-text';
+import * as styles from './edit-floating-button.css';
 
-type ModeSetter = React.Dispatch<React.SetStateAction<boolean>>;
-
-interface RenderActionButtonProps {
-  variant: 'close' | 'edit' | 'complete';
-  icon: JSX.Element;
-  text: string | null;
-  onClick: () => void;
+interface EditFloatingButtonProps {
+  isEditMode: boolean;
+  isEditTimeTableMode: boolean;
+  isFestivalDeleteMode: boolean;
+  isTextVisible: boolean;
+  onToggleEditMode: () => void;
+  onToggleEditTimeTableMode: () => void;
+  onToggleFestivalDeleteMode: () => void;
+  onToggleTextVisibility: (visible: boolean) => void;
+  onResetModes: () => void;
 }
 
-const EditFloatingButton = () => {
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [isEditTimeTableMode, setIsEditTimeTableMode] = useState(false);
-  const [isFestivalDeleteMode, setIsFestivalDeleteMode] = useState(false);
-  const [isTextVisible, setIsTextVisible] = useState(true);
-
-  const resetModes = () => {
-    setIsEditTimeTableMode(false);
-    setIsFestivalDeleteMode(false);
-  };
+const EditFloatingButton = ({
+  isEditMode,
+  isEditTimeTableMode,
+  isFestivalDeleteMode,
+  isTextVisible,
+  onToggleEditMode,
+  onToggleEditTimeTableMode,
+  onToggleFestivalDeleteMode,
+  onToggleTextVisibility,
+  onResetModes,
+}: EditFloatingButtonProps) => {
+  const isAtBottom = useScrollPosition();
+  const adjustedAtBottom =
+    isEditTimeTableMode || isFestivalDeleteMode ? false : isAtBottom;
+  useDisableScroll(isEditMode && !isEditTimeTableMode && !isFestivalDeleteMode);
 
   const handleToggleButton = () => {
-    setIsEditMode((prev) => !prev);
-    setIsTextVisible(true);
-    if (isEditTimeTableMode || isFestivalDeleteMode) resetModes();
-  };
-
-  const handleModeToggle = (modeSetter: ModeSetter): void => {
-    modeSetter((prev: boolean) => !prev);
+    onToggleEditMode();
+    onToggleTextVisibility(true);
+    if (isEditTimeTableMode || isFestivalDeleteMode) onResetModes();
   };
 
   const getBackgroundClassName = () => {
@@ -75,17 +79,32 @@ const EditFloatingButton = () => {
     });
   };
 
+  interface RenderActionButtonProps {
+    variant: 'close' | 'edit' | 'complete';
+    icon: JSX.Element;
+    text: string | null;
+    onClick: () => void;
+  }
+
   const renderActionButton = ({
     variant,
     icon,
     text,
     onClick,
   }: RenderActionButtonProps) => (
-    <button className={styles.buttonVariants({ variant })} onClick={onClick}>
+    <button
+      className={styles.buttonVariants({
+        variant,
+        isAtBottom: adjustedAtBottom,
+      })}
+      onClick={onClick}
+    >
       {icon}
       {text && (
         <span
-          className={`${styles.text} ${isTextVisible ? styles.textVisible : styles.textHidden}`}
+          className={`${styles.text} ${
+            isTextVisible ? styles.textVisible : styles.textHidden
+          }`}
         >
           {text}
         </span>
@@ -96,16 +115,18 @@ const EditFloatingButton = () => {
   const renderActionButtons = () => {
     if (isEditMode && !isEditTimeTableMode && !isFestivalDeleteMode) {
       return (
-        <div className={styles.box}>
+        <div
+          className={`${styles.box} ${adjustedAtBottom ? styles.boxAtBottom : ''}`}
+        >
           <button
-            onClick={() => handleModeToggle(setIsEditTimeTableMode)}
+            onClick={onToggleEditTimeTableMode}
             className={styles.boxButton}
           >
             <IcFloatEdit24 width="2.4rem" height="2.4rem" />
             <span>{EDIT_BOX.EDIT_TIMETABLE}</span>
           </button>
           <button
-            onClick={() => handleModeToggle(setIsFestivalDeleteMode)}
+            onClick={onToggleFestivalDeleteMode}
             className={styles.boxButton}
           >
             <IcFloatDelete24 width="2.4rem" height="2.4rem" />
