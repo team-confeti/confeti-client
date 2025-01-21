e2e9ded1e8471d1d35d7081bca26e64994ac7535
gwagjiug <kwjo0228@naver.com>
2025-01-21 23:16:23 +0900

    feat: 공연정보 검색을 위한 쿼리 키 및 옵션 추가

--- apps/client/src/shared/apis/search/search-queries.ts
+++ apps/client/src/shared/apis/search/search-queries.ts
@@ -1,19 +1,39 @@
 import { queryOptions } from '@tanstack/react-query';
-import { getArtistSearch } from './search';
-export const SEARCH_QUERY_KEY = {
-  ALL: ['search'],
+import { getArtistSearch, getPerformanceSearch } from './search';
+
+export const SEARCH_ARTIST_QUERY_KEY = {
+  ALL: ['artist'],
   SEARCH_ARTIST: (keyword: string) => [
-    ...SEARCH_QUERY_KEY.ALL,
+    ...SEARCH_ARTIST_QUERY_KEY.ALL,
     'keyword',
     keyword,
   ],
 } as const;
 
-export const SEARCH_QUERY_OPTIONS = {
-  ALL: () => queryOptions({ queryKey: SEARCH_QUERY_KEY.ALL }),
+export const SEARCH_ARTIST_QUERY_OPTION = {
+  ALL: () => queryOptions({ queryKey: SEARCH_ARTIST_QUERY_KEY.ALL }),
   SEARCH_ARTIST: (keyword: string, enabled: boolean) => ({
-    queryKey: SEARCH_QUERY_KEY.SEARCH_ARTIST(keyword),
+    queryKey: SEARCH_ARTIST_QUERY_KEY.SEARCH_ARTIST(keyword),
     queryFn: () => getArtistSearch(keyword),
     enabled,
+  }),
+};
+
+// 공연정보 검색
+
+export const SEARCH_PERFOMANCE_QUERY_KEY = {
+  ALL: ['performance'],
+  SEARCH_PERFORMANCE: (artistId: string, cursor: number) => [
+    ...SEARCH_ARTIST_QUERY_KEY.ALL,
+    artistId,
+    cursor,
+  ],
+} as const;
+
+export const SEARCH_PERFOMANCE_QUERY_OPTION = {
+  ALL: () => queryOptions({ queryKey: SEARCH_PERFOMANCE_QUERY_KEY.ALL }),
+  SEARCH_ARTIST: (artistId: string, cursor: number) => ({
+    queryKey: SEARCH_PERFOMANCE_QUERY_KEY.SEARCH_PERFORMANCE(artistId, cursor),
+    queryFn: () => getPerformanceSearch(artistId, cursor),
   }),
 };
