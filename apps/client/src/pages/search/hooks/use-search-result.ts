13d60d6bf4ee60b6fc935adfb30168082f0ae6b2
gwagjiug <kwjo0228@naver.com>
2025-01-21 23:21:33 +0900

    feat: useSearchPerformance 훅 추가 및 검색 쿼리 옵션 임포트

--- apps/client/src/pages/search/hooks/use-search-result.ts
+++ apps/client/src/pages/search/hooks/use-search-result.ts
@@ -1,14 +1,32 @@
 import { useQuery } from '@tanstack/react-query';
-import { SEARCH_ARTIST_QUERY_OPTION } from '@shared/apis/search/search-queries';
+import {
+  SEARCH_ARTIST_QUERY_OPTION,
+  SEARCH_PERFOMANCE_QUERY_OPTION,
+} from '@shared/apis/search/search-queries';
 
 interface UseArtistProps {
   keyword: string;
   enabled: boolean;
 }
+interface UsePerformanceProps {
+  artistId: string;
+  cursor: number;
+}
 
 export const useSearchArtist = ({ keyword, enabled }: UseArtistProps) => {
   const { data } = useQuery({
     ...SEARCH_ARTIST_QUERY_OPTION.SEARCH_ARTIST(keyword, enabled),
+  });
+
+  return data;
+};
+
+export const useSearchPerformance = ({
+  artistId,
+  cursor,
+}: UsePerformanceProps) => {
+  const { data } = useQuery({
+    ...SEARCH_PERFOMANCE_QUERY_OPTION.SEARCH_PERFORMANCE(artistId, cursor),
   });
 
   return data;
