0fc8beab7fb65c1164db90989a137ce913ecc0ef
gwagjiug <kwjo0228@naver.com>
2025-01-21 22:05:49 +0900

    fix: artistResults를 artist로 변경하고 검색 로직 수정

--- apps/client/src/pages/search/page/search.tsx
+++ apps/client/src/pages/search/page/search.tsx
@@ -1,71 +1,59 @@
-import { SearchBar, Spacing, Footer } from '@confeti/design-system';
-import ArtistSection from '../components/artist-section';
-import PerformanceSection from '../components/performance-section';
-import PerformanceCount from '../components/performance-count-section';
-import NoticeSection from '../components/notice-section';
-import ArtistNotFound from '../components/artist-not-found';
-import { useSearch } from '../hooks/use-search';
-import { useSortedPerformances } from '../hooks/use-sorted-performances';
+import React from 'react';
 import * as styles from './search.css';
+import { useNavigate, useSearchParams } from 'react-router-dom'; // useSearchParams 추가
+import { SearchBar, Spacing, Footer } from '@confeti/design-system';
+import { routePath } from '@shared/constants/path';
+import NoticeSection from '@pages/search/components/notice-section';
+import ArtistSection from '@pages/search/components/artist-section';
+import PerformanceSection from '@pages/search/components/performance-section';
+import PerformanceCount from '@pages/search/components/performance-count-section';
+import ArtistNotFound from '@pages/search/components/artist-not-found';
+import { useSearch } from '@pages/search/hooks/use-search';
+import { useSearchArtist } from '../hooks/use-search-result';
 
 const Search = () => {
-  const {
-    searchQuery,
-    setSearchQuery,
-    artistResults,
-    performanceResults,
-    hasSearched,
-    setHasSearched,
-    isSearchBarFocused,
-    setIsSearchBarFocused,
-    handleSearch,
-  } = useSearch();
+  const { searchKeyword, setSearchKeyword } = useSearch();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const keyword = searchKeyword;
 
-  const isMultipleArtists = artistResults[0]?.isMultipleArtists ?? false;
+  const paramsKeyword = searchParams.get('q') || '';
+  const searchData = useSearchArtist({
+    keyword: paramsKeyword,
+    enabled: !!paramsKeyword,
+  });
 
-  const sortedPerformances = useSortedPerformances(performanceResults);
+  const artistData = searchData?.artist ? [searchData.artist] : [];
 
-  const showContent = !isSearchBarFocused && searchQuery !== '' && hasSearched;
-
-  const handleSearchBarChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value);
-    setHasSearched(false);
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(e.target.value);
   };
 
-  const handleSearchBarKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      handleSearch(searchQuery);
+  const handleKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && keyword.trim()) {
+      navigate(`${routePath.SEARCH}?q=${keyword}`);
     }
-  };
-
-  const handleSearchBarFocus = () => {
-    setIsSearchBarFocused(true);
   };
 
   return (
     <>
-      <SearchBar
-        value={searchQuery}
-        onChange={handleSearchBarChange}
-        onKeyDown={handleSearchBarKeyDown}
-        onFocus={handleSearchBarFocus}
-      />
+      <SearchBar onChange={handleOnChange} onKeyDown={handleKeydown} />
       <main className={styles.resultSection}>
-        {showContent ? (
-          artistResults.length > 0 ? (
-            <>
-              <NoticeSection isMultipleArtists={isMultipleArtists} />
-              <ArtistSection artists={artistResults} />
-              <Spacing />
-              <PerformanceCount count={sortedPerformances.length} />
-              <PerformanceSection performances={sortedPerformances} />
-            </>
-          ) : (
-            <ArtistNotFound />
-          )
-        ) : null}
+        {paramsKeyword.length > 0 ? (
+          <>
+            <NoticeSection
+              isMultipleArtists={artistData[0]?.isMultipleArtists}
+            />
+            <ArtistSection artist={artistData} />
+            <Spacing />
+            <PerformanceCount />
+            <PerformanceSection />
+          </>
+        ) : (
+          <ArtistNotFound />
+        )}
       </main>
-      {showContent && <Footer />}
+      <Footer />
     </>
   );
 };
