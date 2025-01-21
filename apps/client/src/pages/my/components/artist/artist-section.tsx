466e40cb060f988644241ab9412d688cd7a02d5f
hanseo <yds06206@gmail.com>
2025-01-21 23:12:22 +0900

    feat: 마이페이지에도 조회api 연결

--- apps/client/src/pages/my/components/artist/artist-section.tsx
+++ apps/client/src/pages/my/components/artist/artist-section.tsx
@@ -1,6 +1,6 @@
 import { ArtistCard } from '@confeti/design-system';
 import * as styles from './artist-section.css';
-
+import { useMyArtistQuery } from '@pages/my/hooks/use-my-artist-query';
 type Artist = {
   artistId: string;
   name: string;
@@ -12,9 +12,10 @@ interface ArtistSectionProps {
 }
 
 const ArtistSection = ({ artists }: ArtistSectionProps) => {
+  const { data: artistData } = useMyArtistQuery();
   return (
     <div className={styles.container}>
-      {artists.map((artist) => (
+      {artistData?.artists.map((artist) => (
         <ArtistCard
           key={artist.artistId}
           artistId={artist.artistId}
