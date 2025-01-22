6a1c4f8f62548409e82658f467ac8f1bc5c9b482
gwagjiug <kwjo0228@naver.com>
2025-01-23 03:42:30 +0900

    feat: FESTIVAL_TIMETABLE 쿼리 옵션에 활성화 조건 추가

--- apps/client/src/shared/apis/time-table/festival-timetable-queries.ts
+++ apps/client/src/shared/apis/time-table/festival-timetable-queries.ts
@@ -6,9 +6,14 @@ export const FESTIVAL_TIMETABLE_QUERY_KEY = {
 } as const;
 
 export const FESTIVAL_TIMETABLE_QUERY_OPTIONS = {
-  FESTIVAL_TIMETABLE: (festivalDateId: number) =>
-    queryOptions({
+  FESTIVAL_TIMETABLE: (festivalDateId: number) => {
+    const options = queryOptions({
       queryKey: [...FESTIVAL_TIMETABLE_QUERY_KEY.ALL, festivalDateId],
       queryFn: () => getFestivalTimetable(festivalDateId),
-    }),
+    });
+    return {
+      ...options,
+      enabled: !!festivalDateId,
+    };
+  },
 };
