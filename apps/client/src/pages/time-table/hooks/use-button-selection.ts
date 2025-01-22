e25c14707e5405cb4ba28bd83b24cd505def1253
gwagjiug <kwjo0228@naver.com>
2025-01-23 04:43:42 +0900

    fix: 콘솔 로그 제거

--- apps/client/src/pages/time-table/hooks/use-button-selection.ts
+++ apps/client/src/pages/time-table/hooks/use-button-selection.ts
@@ -42,7 +42,6 @@ export const useButtonSelection = (festivals: Festival[]) => {
 
   const handleFestivalClick = (festivalId: number) => {
     setClickedFestivalId(festivalId);
-    console.log(1);
     const newDates = festivalDatesMap.get(festivalId) || [];
     setSelectedFestivalDateId(
       newDates.length > 0 ? newDates[0].festivalDateId : null,
