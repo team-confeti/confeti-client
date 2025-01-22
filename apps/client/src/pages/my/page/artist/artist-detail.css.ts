cdef16a4517827b8b0d14fbe8693dda3b3370611
hanseo <yds06206@gmail.com>
2025-01-23 04:12:51 +0900

    fix: 스타일 수정

--- apps/client/src/pages/my/page/artist/artist-detail.css.ts
+++ apps/client/src/pages/my/page/artist/artist-detail.css.ts
@@ -3,6 +3,8 @@ import { style } from '@vanilla-extract/css';
 export const container = style({
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
+  alignItems: 'center',
+  justifyItems: 'center',
   padding: '2.4rem 3rem',
   gap: '3.8rem',
   gridRowGap: '3rem',
