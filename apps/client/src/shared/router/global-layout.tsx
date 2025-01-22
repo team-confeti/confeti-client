d0c99d3ff875844025d6cbbe0d1f22c282ba81f7
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-01-22 21:12:19 +0900

    feat: 스크롤 위치 초기화 컴포넌트 추가

--- apps/client/src/shared/router/global-layout.tsx
+++ apps/client/src/shared/router/global-layout.tsx
@@ -2,14 +2,15 @@ import { Outlet } from 'react-router-dom';
 import { Header } from '@confeti/design-system';
 import { Suspense } from 'react';
 import Loading from '@shared/pages/loading/loading';
+import ScrollToTop from './scroll-to-top';
 
 export default function GlobalLayout() {
   return (
-    <>
+    <ScrollToTop>
       <Header />
       <Suspense fallback={<Loading />}>
         <Outlet />
       </Suspense>
-    </>
+    </ScrollToTop>
   );
 }
