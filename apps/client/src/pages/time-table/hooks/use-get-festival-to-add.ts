5f847db6620f1da9d9ca882fcdd4b7a9b8a96744
daahyunk <ekgusdl0408@naver.com>
2025-01-23 01:56:10 +0900

    chore: 불필요한 주석 삭제

--- apps/client/src/pages/time-table/hooks/use-get-festival-to-add.ts
+++ apps/client/src/pages/time-table/hooks/use-get-festival-to-add.ts
@@ -5,7 +5,6 @@ export const useGetFestivalToAdd = () => {
   const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
     ...GET_FESTIVAL_TO_ADD_QUERY_OPTIONS.LIST(),
     getNextPageParam: (lastPage) => {
-      // nextCursor가 -1이면 더 이상 데이터가 없음을 의미
       return lastPage.nextCursor === -1 ? undefined : lastPage.nextCursor;
     },
   });
