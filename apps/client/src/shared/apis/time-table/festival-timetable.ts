62ed64243abfd9395c6bb30b4bd97c2dd5b25dd3
gwagjiug <kwjo0228@naver.com>
2025-01-23 04:45:40 +0900

    fix: FestivalTimetableResponse 임포트 정리

--- apps/client/src/shared/apis/time-table/festival-timetable.ts
+++ apps/client/src/shared/apis/time-table/festival-timetable.ts
@@ -1,7 +1,9 @@
 import { BaseResponse } from '@shared/types/api';
-import { FestivalTimetableResponseExtended } from '@shared/types/festival-timetable-response';
+import {
+  FestivalTimetableResponse,
+  FestivalTimetableResponseExtended,
+} from '@shared/types/festival-timetable-response';
 import { END_POINT } from '@shared/constants/api';
-import { get, del } from '../config/instance';
 import { get, del } from '../config/instance';
 
 export const getFestivalTimetable = async (
