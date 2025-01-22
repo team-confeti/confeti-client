014f2bf80a50a9131d176e1d8d2b3a3087ca4fa8
gwagjiug <kwjo0228@naver.com>
2025-01-22 20:55:26 +0900

    fix: 공연 상세 조회 API에 콘서트 ID 매개변수 추가

--- apps/client/src/pages/my/hooks/use-performance-detail.ts
+++ apps/client/src/pages/my/hooks/use-performance-detail.ts
@@ -1,7 +1,9 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
-import { PERFORMANCE_QUERY_OPTIONS } from '@shared/apis/user-queries';
+import { PERFORMANCE_QUERY_OPTIONS } from '@shared/apis/confeti-detail/performance-queries';
 
-export const useUserPerformanceMore = () => {
-  const { data } = useSuspenseQuery(PERFORMANCE_QUERY_OPTIONS.ALL());
+export const useUserPerformanceMore = (concertId: number) => {
+  const { data } = useSuspenseQuery(
+    PERFORMANCE_QUERY_OPTIONS.CONCERT(concertId),
+  );
   return data;
 };
