9916dd83bc5d224433fff272ab173b020d00a41d
gwagjiug <kwjo0228@naver.com>
2025-01-23 01:42:49 +0900

    feat: 축제 관련 훅 및 목업 데이터 삭제

--- apps/client/src/pages/time-table/hooks/use-festival-timetables.ts
+++
@@ -1,9 +0,0 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
-import { FESTIVAL_BUTTON_QUERY_OPTIONS } from '@shared/apis/time-table/festival-button-queries';
-
-export const useFestivalTimetables = () => {
-  const { data } = useSuspenseQuery(
-    FESTIVAL_BUTTON_QUERY_OPTIONS.FESTIVAL_BUTTON(),
-  );
-  return data;
-};
