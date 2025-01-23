51e516a79497589cf734db0298f5ca96e9597b3c
ce. <154000318+bongtta@users.noreply.github.com>
2025-01-24 07:00:22 +0900

    fix: 디데이 계산 로직 수정 (#245)

--- packages/design-system/src/components/ticketing-carousel/hooks/use-data-format.ts
+++ packages/design-system/src/components/ticketing-carousel/hooks/use-data-format.ts
@@ -9,6 +9,11 @@ export const useDateFormat = (reserveAt: string) => {
   const calculateDday = () => {
     const currentDate = new Date();
     const targetDate = new Date(reserveAt.split('.').join('-')); // 같은 형식으로 변환
+
+    // 시간 초기화 (00:00:00)
+    currentDate.setHours(0, 0, 0, 0);
+    targetDate.setHours(0, 0, 0, 0);
+
     const timeDifference = targetDate.getTime() - currentDate.getTime();
     const remainingDays = Math.ceil(timeDifference / (1000 * 3600 * 24)); // 남은 일수 계산
 
