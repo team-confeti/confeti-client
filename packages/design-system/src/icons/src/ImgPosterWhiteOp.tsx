99634fe9a51c8b675716a088498905790288d1b3
hanseo <yds06206@gmail.com>
2025-01-24 00:41:58 +0900

    feat: svgr로 변환

--- packages/design-system/src/icons/src/ImgPosterWhiteOp.tsx
+++ packages/design-system/src/icons/src/ImgPosterWhiteOp.tsx
@@ -5,7 +5,6 @@ const SvgImgPosterWhiteOp = (props: SVGProps<SVGSVGElement>) => (
     width="1em"
     height="1em"
     fill="none"
-    viewBox="0 0 156 208"
     {...props}
   >
     <path fill="#fff" fillOpacity={0.3} d="M0 0h156v208H0z" />
