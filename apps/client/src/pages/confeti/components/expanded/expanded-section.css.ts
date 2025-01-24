bc0e53f11d1b02ab39be9010abbd16dac8da1ae7
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-01-25 04:28:49 +0900

    feat: 아티스트 목록 확장 시 애니메이션 효과 추가

--- apps/client/src/pages/confeti/components/expanded/expanded-section.css.ts
+++ apps/client/src/pages/confeti/components/expanded/expanded-section.css.ts
@@ -3,9 +3,18 @@ import { themeVars } from '@confeti/design-system/styles';
 
 export const expandedSection = style({
   ...themeVars.display.flexColumn,
+  position: 'relative',
 });
 
 export const expandedArtists = style({
-  backgroundColor: themeVars.color.gray100,
+  backgroundColor: themeVars.color.gray200,
+  overflow: 'hidden',
+  transition: 'height 0.4s ease-out, opacity 0s ease-out',
+  opacity: 0,
+  padding: '0 2rem',
+});
+
+export const expandedArtistsVisible = style({
+  opacity: 1,
   padding: '2rem',
 });
