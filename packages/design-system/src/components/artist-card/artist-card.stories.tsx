10861b973543c274c9ac31d8889373587c5c1073
gwagjiug <kwjo0228@naver.com>
2025-02-11 00:22:37 +0900

    refactor: CSS 스타일 수정 및 스토리북 데이터 업데이트

--- packages/design-system/src/components/artist-card/artist-card.stories.tsx
+++ packages/design-system/src/components/artist-card/artist-card.stories.tsx
@@ -9,7 +9,7 @@ const meta: Meta<typeof ArtistCard> = {
   },
   decorators: [
     (Story) => (
-      <div style={{ width: '375px' }}>
+      <div style={{ width: '100px' }}>
         <Story />
       </div>
     ),
@@ -24,26 +24,29 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     artistId: '1',
-    title: '데이식스',
-    imageSrc: 'https://dummyimage.com/80X80',
-    size: 'lg',
+    title: '한로로',
+    imageSrc:
+      'https://i.scdn.co/image/ab6761610000f1786a50f39b95ce98a0e6bf5b21',
+    size: 'md',
   },
 };
 
 export const Sm: Story = {
   args: {
-    artistId: '1',
-    title: '데이식스',
-    imageSrc: 'https://dummyimage.com/80X80',
+    artistId: '2',
+    title: 'Coldplay',
+    imageSrc:
+      'https://i.scdn.co/image/ab6761610000f1781ba8fc5f5c73e7e9313cc6eb',
     size: 'sm',
   },
 };
 
 export const Md: Story = {
   args: {
-    artistId: '1',
+    artistId: '3',
     title: '데이식스',
-    imageSrc: 'https://dummyimage.com/80X80',
+    imageSrc:
+      'https://i.scdn.co/image/ab6761610000f17810e83b0ca558533d0f3c376c',
     size: 'md',
   },
 };
