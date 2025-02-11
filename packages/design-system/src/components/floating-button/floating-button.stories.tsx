1ee9a7a41ceac0206f9fe7ba7eac15ed650563a6
gwagjiug <kwjo0228@naver.com>
2025-02-12 03:21:19 +0900

    fix: 스토리북에서 LikeButton의 렌더링을 개선하고 스타일을 추가

--- packages/design-system/src/components/floating-button/floating-button.stories.tsx
+++ packages/design-system/src/components/floating-button/floating-button.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import FloatingButton from './floating-button';
-
 const meta: Meta<typeof FloatingButton> = {
   title: 'Common/FloatingButton',
   component: FloatingButton,
