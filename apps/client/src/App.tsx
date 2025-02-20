31ff228bae8ca634534f09ae7575eb60d5271c57
gwagjiug <kwjo0228@naver.com>
2025-02-21 00:51:19 +0900

    chore: Sentry 초기화 설정 추가

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -3,11 +3,18 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider, ToastContainer } from '@confeti/design-system';
 import { rootStyle } from '@confeti/design-system/styles';
-
+import * as Sentry from '@sentry/react';
 import Router from '@shared/router/router';
 import { queryClient } from './shared/utils/query-client';
 
 function App() {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [Sentry.browserTracingIntegration()],
+    tracePropagationTargets: ['localhost', /^https:\/\/confeti\.co\.kr/],
+    tracesSampleRate: 1.0,
+  });
+
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
