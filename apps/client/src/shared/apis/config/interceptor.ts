8b3d93a5ea781c3539cb203a1a0c221b86cf9c18
gwagjiug <kwjo0228@naver.com>
2025-02-26 17:13:24 +0900

    chore: API 오류 및 네트워크 오류 처리 시 Sentry 로깅 추가

--- apps/client/src/shared/apis/config/interceptor.ts
+++ apps/client/src/shared/apis/config/interceptor.ts
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/react';
 import { HTTP_STATUS_CODE } from '@shared/constants/api';
 import { AxiosError, InternalAxiosRequestConfig } from 'axios';
 import { HTTPError } from './http-error';
@@ -9,9 +10,44 @@ interface ErrorResponse {
 }
 
 export const handleAPIError = (error: AxiosError<ErrorResponse>) => {
-  if (!error.response) throw error;
+  if (!error.response) {
+    Sentry.withScope((scope) => {
+      scope.setLevel('fatal');
+      scope.setTag('error_type', 'network_error');
+      scope.captureMessage(`[네트워크 오류] ${window.location.href}`);
 
-  const { data, status } = error.response;
+      scope.setContext('네트워크 오류 정보', {
+        message: error.message,
+        code: error.code,
+        config: error.config,
+      });
+    });
+
+    throw new HTTPError(0, '네트워크 오류 발생');
+  }
+
+  const { config, response } = error;
+  const { data, status, headers } = response;
+
+  Sentry.withScope((scope) => {
+    scope.setLevel('error');
+    scope.setTag('error_type', 'api_error');
+    scope.setTag('API URL', config?.url || 'unknown');
+    scope.setTag('HTTP Method', config?.method || 'unknown');
+    scope.setTag('Status Code', status.toString());
+
+    scope.captureMessage(`[API 오류] ${window.location.href}`);
+
+    scope.setContext('API 요청 정보', {
+      method: config?.method,
+      url: config?.url,
+      params: config?.params,
+      data: config?.data,
+      headers: config?.headers,
+    });
+
+    scope.setContext('응답 데이터', { status, headers, data });
+  });
 
   if (status >= HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR) {
     throw new HTTPError(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR, data.message);
