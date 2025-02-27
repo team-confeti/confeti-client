3b63114f549bd8021efdf6f203277a2525771165
gwagjiug <kwjo0228@naver.com>
2025-02-27 21:30:10 +0900

    fix: 에러 경계 처리에서 잘못된 컴포넌트 이름 수정

--- apps/client/src/shared/router/global-layout.tsx
+++ apps/client/src/shared/router/global-layout.tsx
@@ -4,13 +4,13 @@ import { Header } from '@confeti/design-system';
 import { Suspense } from 'react';
 import Loading from '@shared/pages/loading/loading';
 import ScrollToTop from './scroll-to-top';
-import ErroFallback from '@shared/pages/error/error';
+import ErrorFallback from '@shared/pages/error/error';
 
 export default function GlobalLayout() {
   return (
     <ScrollToTop>
       <Header />
-      <Sentry.ErrorBoundary fallback={ErroFallback}>
+      <Sentry.ErrorBoundary fallback={ErrorFallback}>
         <Suspense fallback={<Loading />}>
           <Outlet />
         </Suspense>
