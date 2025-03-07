f6990600a1cfef427528dd3fa89927c22a32a254
daahyunk <ekgusdl0408@naver.com>
2025-03-07 18:07:38 +0900

    chore: import order 제거

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:prettier/recommended',
   ],
-  plugins: ['import', 'simple-import-sort'],
+  plugins: ['simple-import-sort'],
   parser: '@typescript-eslint/parser',
   settings: {
     react: {
@@ -17,20 +17,6 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
 
-    'import/order': [
-      'error',
-      {
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-        ],
-        'newlines-between': 'always',
-      },
-    ],
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
   },
