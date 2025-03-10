1da1b978d431fa7d950ebb0f6da7924b9e73faac
gwagjiug <kwjo0228@naver.com>
2025-03-11 01:44:53 +0900

    delete: type-guards.ts 파일 삭제 및 관련 타입 가드 함수 제거

--- apps/client/src/pages/time-table/types/type-guards.ts
+++
@@ -1,7 +0,0 @@
-export const isString = (value: number | string): value is string => {
-  return typeof value === 'string';
-};
-
-export const isNumber = (value: number | string): value is number => {
-  return typeof value === 'number';
-};
