be3ad77c5b5619ce7734f9046c9c9c26c3e45f13
gwagjiug <kwjo0228@naver.com>
2025-03-12 02:07:05 +0900

    feat: 로그인 페이지에 카카오 버튼 추가

--- apps/client/src/pages/login/page/login.tsx
+++ apps/client/src/pages/login/page/login.tsx
@@ -1,12 +1,16 @@
 import { Header } from '@confeti/design-system';
 import { BtnDeleteBlack20 } from '@confeti/design-system/icons';
+import { Button } from '@confeti/design-system';
 const login = () => {
   return (
-    <Header
-      variant="detail"
-      title="로그인"
-      icon={<BtnDeleteBlack20 width={'2rem'} height={'2rem'} />}
-    />
+    <section>
+      <Header
+        variant="detail"
+        title="로그인"
+        icon={<BtnDeleteBlack20 width={'2rem'} height={'2rem'} />}
+      />
+      <Button text="카카오로 계속하기" variant="kakao" />
+    </section>
   );
 };
 
