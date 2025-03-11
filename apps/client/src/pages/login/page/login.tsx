198cdfe2aefaaf2e2427200b0a9bac3142a8daba
gwagjiug <kwjo0228@naver.com>
2025-03-12 03:28:35 +0900

    feat: 로그인 페이지에 스타일 및 애플 로그인 버튼 추가

--- apps/client/src/pages/login/page/login.tsx
+++ apps/client/src/pages/login/page/login.tsx
@@ -1,17 +1,59 @@
 import { Header } from '@confeti/design-system';
-import { BtnDeleteBlack20 } from '@confeti/design-system/icons';
+import {
+  BtnDeleteBlack20,
+  IcKakao,
+  IcApple,
+  ImgTypelogoBig,
+  Confeti3DLogo21,
+} from '@confeti/design-system/icons';
 import { Button } from '@confeti/design-system';
-const login = () => {
+import * as styles from './login.css';
+import { routePath } from '@shared/constants/path';
+
+const DESCRIPTION = {
+  atagText: ['이용약관 ', ' 개인정보처리방침'],
+  description: ['가입 시, confeti의', ' 및', '에 동의하게 돼요.'],
+};
+
+const Login = () => {
   return (
-    <section>
+    <>
       <Header
         variant="detail"
         title="로그인"
         icon={<BtnDeleteBlack20 width={'2rem'} height={'2rem'} />}
       />
-      <Button text="카카오로 계속하기" variant="kakao" />
-    </section>
+      <section className={styles.container}>
+        <div className={styles.Logo}>
+          <Confeti3DLogo21 width={'18rem'} height={'18rem'} />
+          <ImgTypelogoBig width={'17rem'} height={'4rem'} />
+        </div>
+        <div className={styles.loginButtonSection}>
+          <Button
+            text="카카오로 계속하기"
+            variant="kakao"
+            icon={<IcKakao width={'2.4rem'} height={'2.4rem'} />}
+          />
+          <Button
+            text="애플로 계속하기"
+            variant="apple"
+            icon={<IcApple width={'2.4rem'} height={'2.4rem'} />}
+          />
+        </div>
+      </section>
+      <div className={styles.description}>
+        <p>{DESCRIPTION.description[0]}</p>
+        <a href={routePath.ROOT} className={styles.atagText}>
+          {DESCRIPTION.atagText[0]}
+        </a>
+        {DESCRIPTION.description[1]}
+        <a href={routePath.ROOT} className={styles.atagText}>
+          {DESCRIPTION.atagText[1]}
+        </a>
+        {DESCRIPTION.description[2]}
+      </div>
+    </>
   );
 };
 
-export default login;
+export default Login;
