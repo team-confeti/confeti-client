1250e6e5179e03c27f20faa64d8cf7a208be4e03
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-03-11 22:56:38 +0900

    refactor: toast 함수 리팩토링 및 고유 ID 생성 방식 변경

--- packages/design-system/src/components/toast/utils/toast.ts
+++ packages/design-system/src/components/toast/utils/toast.ts
@@ -1,25 +1,15 @@
-import { ToastEvent, ToastProps, ToastType } from '../types/type';
+import { ToastEvent, ToastProps } from '../types';
 import { eventManager } from './eventManager';
 
-type ToastAddFunctionProps = Omit<ToastProps, 'toastId' | 'type'>;
-type ToastOptions = Omit<ToastProps, 'toastId' | 'type' | 'text'>;
-const generateUniqueId = () => {
-  return Date.now().toString(36) + Math.random().toString(36).substring(4);
-};
+type ToastOptions = Omit<ToastProps, 'toastId' | 'type'>;
 
-const emitAddToast = (type: ToastType, toastProps: ToastAddFunctionProps) => {
-  const id = generateUniqueId();
+const emitAddToast = (toastProps: ToastOptions) => {
+  const id = crypto.randomUUID();
   eventManager.emit(ToastEvent.Add, {
     ...toastProps,
     toastId: id,
-    type,
   });
   return id;
 };
 
-export const toast = {
-  default: (text: string, toastOptions?: ToastOptions) =>
-    emitAddToast('default', { text: text, ...toastOptions }),
-  success: (text: string, toastOptions?: ToastOptions) =>
-    emitAddToast('success', { text: text, ...toastOptions }),
-};
+export const toast = (options: ToastOptions) => emitAddToast(options);
