f1a83d386247cd5c9df1a46c9f03d7f0b701b09d
gwagjiug <kwjo0228@naver.com>
2025-03-12 02:09:34 +0900

    feat: 새로운 아이콘 및 로고 컴포넌트 추가

---
+++ packages/design-system/src/icons/src/IcApple.tsx
@@ -0,0 +1,17 @@
+import type { SVGProps } from 'react';
+const SvgIcApple = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="#212224"
+      d="M17.05 19.28c-.98.95-2.05.8-3.08.35-1.09-.46-2.09-.48-3.24 0-1.44.62-2.2.44-3.06-.35C2.79 14.25 3.51 6.59 9.05 6.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.51.12 2.65.72 3.4 1.8-3.12 1.87-2.38 5.98.48 7.13-.57 1.5-1.31 2.99-2.54 4.09zM12.03 6.25c-.15-2.23 1.66-4.07 3.74-4.25.29 2.58-2.34 4.5-3.74 4.25"
+    />
+  </svg>
+);
+export default SvgIcApple;
