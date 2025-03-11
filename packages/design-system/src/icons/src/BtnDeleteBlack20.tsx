f2c860f448d1bd3ba58995608668301f48e16f06
gwagjiug <kwjo0228@naver.com>
2025-03-12 00:09:56 +0900

    feat: BtnDeleteBlack20 아이콘 컴포넌트 추가

---
+++ packages/design-system/src/icons/src/BtnDeleteBlack20.tsx
@@ -0,0 +1,17 @@
+import type { SVGProps } from 'react';
+const SvgBtnDeleteBlack20 = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 20 20"
+    {...props}
+  >
+    <path
+      fill="#121212"
+      d="m15.833 5.342-1.175-1.175-4.659 4.658-4.658-4.658-1.175 1.175L8.824 10 4.166 14.66l1.175 1.175 4.658-4.659 4.659 4.659 1.175-1.175L11.174 10z"
+    />
+  </svg>
+);
+export default SvgBtnDeleteBlack20;
