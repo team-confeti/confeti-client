71b3a3c43e58c49259e4351b9601501188f720f4
gwagjiug <kwjo0228@naver.com>
2025-03-16 01:26:43 +0900

    feat: 쿠키 관리를 위한 유틸 함수 추가

---
+++ apps/client/src/shared/utils/use-cookie.ts
@@ -0,0 +1,29 @@
+import {
+  ACCESS_TOKEN_KEY,
+  REFRESH_TOKEN_KEY,
+} from '@shared/constants/user-constants';
+import Cookies from 'js-cookie';
+
+export const cookieUtil = (
+  action: 'set' | 'remove',
+  accessToken?: string,
+  refreshToken?: string,
+) => {
+  switch (action) {
+    case 'set':
+      if (accessToken)
+        Cookies.set(ACCESS_TOKEN_KEY, accessToken, { path: '/', secure: true });
+      if (refreshToken)
+        Cookies.set(REFRESH_TOKEN_KEY, refreshToken, {
+          path: '/',
+          secure: true,
+        });
+      break;
+    case 'remove':
+      Cookies.remove(ACCESS_TOKEN_KEY, { path: '/' });
+      Cookies.remove(REFRESH_TOKEN_KEY, { path: '/' });
+      break;
+    default:
+      throw new Error('유효한 action 값을 입력해주세요');
+  }
+};
