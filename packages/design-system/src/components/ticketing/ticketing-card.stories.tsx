326b4ca30f52ca033a1ca956d696b115fcceb16c
gwagjiug <kwjo0228@naver.com>
2025-03-17 22:31:29 +0900

    feat: 티켓팅 카드 컴포넌트 및 스토리 추가

---
+++ packages/design-system/src/components/ticketing/ticketing-card.stories.tsx
@@ -0,0 +1,7 @@
+import { Meta } from 'react-router-dom';
+import ticketingCard from './ticketing-card';
+
+export default {
+  title: 'Common/Ticketing/TicketingCard',
+  component: ticketingCard,
+};
---
+++ packages/design-system/src/components/ticketing/ticketing-card.tsx
@@ -0,0 +1,5 @@
+const ticketingCard = () => {
+  return <div></div>;
+};
+
+export default ticketingCard;
