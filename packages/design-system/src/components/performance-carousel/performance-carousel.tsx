413a3b54a9a2584e8b234e31cca0c948afa215c0
hanseo <yds06206@gmail.com>
2025-03-18 19:26:12 +0900

    refactor: 캐러셀에 context 사용한 컴파운드 패턴 적용

--- packages/design-system/src/components/performance-carousel/performance-carousel.tsx
+++ packages/design-system/src/components/performance-carousel/performance-carousel.tsx
@@ -179,6 +179,7 @@ const ImageSlider = ({ children }: { children?: ReactNode }) => {
   );
 };
 
+// 서브컴포넌트 연결
 PerformanceCarousel.Info = Info;
 PerformanceCarousel.Badge = Badge;
 PerformanceCarousel.ImageSlider = ImageSlider;
