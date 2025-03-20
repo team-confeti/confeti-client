9448494e0e6a70c102993b2e0e827e9aa2483d61
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-03-20 17:06:32 +0900

    Refactor(design-system): Toast 컴포넌트 DX 개선 및 최적화 (#327)
    * refactor: toast 함수 리팩토링 및 고유 ID 생성 방식 변경
    
    * refactor: 토스트 타입 정의 파일명 변경
    
    * feat: 위치에 따른 애니메이션 효과 적용 및 icon, cn Props추가
    
    * refactor: 토스트 위치 스타일 개선
    
    * feat: ToastContainer 컴포넌트 메모이제이션 적용
    
    * feat: 기본 토스트 위치를 상수로 정의
    
    * feat: 토스트 생성 함수에 문자열 옵션 지원 추가
    
    * feat: 이벤트 매니저의 콜백 타입을 개선 및 토스트 위치 처리 로직 최적화
    
    * refactor: ToastItem 컴포넌트를 Toast로 변경
    
    * refactor: useToastContainer 훅 확장자 변경
    
    * refactor: 토스트 컴포넌트 props 수정
    
    * refactor: useToastContainer 훅 export 형식 변경
    
    * feat: useToast 커스텀 훅 추가
    
    * refactor: 스토리북 Toast 컴포넌트 props 추가 및 예시 수정
    
    * feat: 최대 활성 토스트 수 제한
    
    * feat: 토스트 관련 파일 케밥케이스 적용
    
    * fix: 파일명 오타 수정
    
    * fix: 오타 수정
    
    * feat: 수정된 toast UI 적용
    
    * refactor: 변경된 toast 적용
    
    * fix: lint 적용

--- packages/design-system/src/components/toast/toast.css.ts
+++ packages/design-system/src/components/toast/toast.css.ts
@@ -13,9 +13,14 @@ const toastExitToBottom = keyframes({
   '100%': { opacity: 0, transform: 'translateY(100%)' },
 });
 
-export const icon = style({
-  width: '2.4rem',
-  height: '2.4rem',
+const toastEnterFromTop = keyframes({
+  '0%': { opacity: 0, transform: 'translateY(-100%)' },
+  '100%': { opacity: 1, transform: 'translateY(0)' },
+});
+
+const toastExitToTop = keyframes({
+  '0%': { opacity: 1, transform: 'translateY(0)' },
+  '100%': { opacity: 0, transform: 'translateY(-100%)' },
 });
 
 export const content = style({
@@ -23,37 +28,80 @@ export const content = style({
   ...themeVars.display.flexJustifyAlignCenter,
 });
 
-export const toast = recipe({
+export const text = style({
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'pre-line',
+  maxWidth: '28rem',
+});
+
+export const highlightText = style({
+  color: themeVars.color.confeti_lime3,
+  display: 'inline-block',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+  maxWidth: '33vw',
+  verticalAlign: 'bottom',
+});
+
+export const toastVariants = recipe({
   base: {
-    position: 'fixed',
-    width: '32.5rem',
-    height: '5rem',
+    width: '89%',
+    '@media': {
+      'screen and (min-width: 430px)': {
+        width: '39rem',
+      },
+    },
+
+    position: 'relative',
+    padding: '1.1rem 0.8rem',
 
     ...themeVars.display.flexJustifyAlignCenter,
-    transform: 'translateX(-50%)',
-    borderRadius: '5rem',
-    backgroundColor: themeVars.color.gray800,
-    color: themeVars.color.confeti_lime2,
+    borderRadius: 20,
+    backgroundColor: themeVars.color.white,
+    color: themeVars.color.black,
     textAlign: 'center',
     zIndex: themeVars.zIndex.toast.content,
-    ...themeVars.fontStyles.body2_m_15,
+    boxShadow: '0px 0px 6px 0px rgba(0, 0, 0, 0.15)',
+    ...themeVars.fontStyles.body3_m_14,
+
+    cursor: 'pointer',
+    pointerEvents: 'auto',
+    touchAction: 'auto',
   },
   variants: {
-    position: {
-      topCenter: { top: '0rem' },
-      bottomCenter: { bottom: '0rem' },
-      middleCenter: { bottom: '0rem' },
+    isTopPosition: {
+      true: {},
+      false: {},
     },
     animation: {
-      enter: {
-        animation: `${toastEnterFromBottom} 0.5s ease-out forwards`,
-      },
-      exit: {
-        animation: `${toastExitToBottom} 0.5s ease-in forwards`,
-      },
+      enter: {},
+      exit: {},
     },
   },
+  compoundVariants: [
+    // Top Position + Animation
+    {
+      variants: { isTopPosition: true, animation: 'enter' },
+      style: { animation: `${toastEnterFromTop} 0.5s ease-out forwards` },
+    },
+    {
+      variants: { isTopPosition: true, animation: 'exit' },
+      style: { animation: `${toastExitToTop} 0.5s ease-in forwards` },
+    },
+
+    // Bottom/Middle Position + Animation
+    {
+      variants: { isTopPosition: false, animation: 'enter' },
+      style: { animation: `${toastEnterFromBottom} 0.5s ease-out forwards` },
+    },
+    {
+      variants: { isTopPosition: false, animation: 'exit' },
+      style: { animation: `${toastExitToBottom} 0.5s ease-in forwards` },
+    },
+  ],
   defaultVariants: {
-    animation: 'enter',
+    isTopPosition: false,
   },
 });
