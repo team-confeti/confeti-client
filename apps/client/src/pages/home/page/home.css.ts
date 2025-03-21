864764e1130a120278622d71076e12f72540b1ba
gwagjiug <kwjo0228@naver.com>
2025-03-22 02:19:43 +0900

    feat: 배경 색상을 퍼플에서 라임 그라디언트로 변경

--- apps/client/src/pages/home/page/home.css.ts
+++ apps/client/src/pages/home/page/home.css.ts
@@ -3,7 +3,7 @@ import { style } from '@vanilla-extract/css';
 import { themeVars } from '@confeti/design-system/styles';
 
 export const background = style({
-  background: themeVars.color.confeti_purple,
+  background: themeVars.color.confeti_lime_grad,
 });
 
 export const ticketingCardContainer = style({
@@ -18,7 +18,7 @@ export const ticketingCardContainer = style({
 export const performanceBannerContainer = style({
   width: '100%',
   paddingTop: '0.1rem',
-  background: themeVars.color.confeti_purple_grad,
+  background: themeVars.color.confeti_lime_grad,
 });
 
 export const ticketingBannerContainer = style({
