2315c0f5a1f344e55c892325c189c51306c56885
bongtta <kce0419@naver.com>
2025-03-26 08:02:29 +0900

    chore: 스토리북 내용 수정

--- packages/design-system/src/components/dropdown/dropdown-button.stories.tsx
+++ packages/design-system/src/components/dropdown/dropdown-button.stories.tsx
@@ -6,14 +6,14 @@ const meta: Meta<typeof DropdownButton> = {
   title: 'Common/DropdownButton',
   component: DropdownButton,
   parameters: {
-    componentSubtitle: '더보기 버튼 컴포넌트',
+    componentSubtitle: '드롭다운 버튼 컴포넌트',
     layout: 'centered',
     docs: {
       description: {
         component:
-          'DropdownButton은 추가 옵션을 표시하는 Kebab(더보기) 버튼 컴포넌트입니다.\n\n' +
+          'DropdownButton은 페스티벌 추가 또는 삭제 옵션을 표시하는 Kebab 버튼 컴포넌트입니다.\n\n' +
           '1. **isActive**: 버튼이 활성화된 상태인지 여부를 나타냅니다.\n' +
-          '2. **onToggle**: 버튼 클릭 시 활성화 상태를 변경합합니다.\n\n',
+          '2. **onToggle**: 버튼 클릭 시 활성화 상태를 변경합니다.\n\n',
       },
     },
   },
