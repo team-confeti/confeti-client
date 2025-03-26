3fd389630c2e1a9a98d88814a70699c4d200105b
gwagjiug <kwjo0228@naver.com>
2025-03-26 17:27:22 +0900

    refactor: like API 엔드포인트 수정

--- apps/client/src/shared/apis/like/like.ts
+++ apps/client/src/shared/apis/like/like.ts
@@ -4,15 +4,11 @@ import { BaseResponseWithoutData } from '@shared/types/api';
 import { del, post } from '../config/instance';
 
 export const postLikeArtist = async (artistId: string): Promise<void> => {
-  await post<BaseResponseWithoutData>(
-    `${END_POINT.GET_FAVORITE_ARTISTS}/${artistId}`,
-  );
+  await post<BaseResponseWithoutData>(END_POINT.POST_LIKE_ARTIST(artistId));
 };
 
 export const deleteLikeArtist = async (artistId: string): Promise<void> => {
-  await del<BaseResponseWithoutData>(
-    `${END_POINT.GET_FAVORITE_ARTISTS}/${artistId}`,
-  );
+  await post<BaseResponseWithoutData>(END_POINT.POST_LIKE_ARTIST(artistId));
 };
 
 export const postLikeFestival = async (festivalId: number): Promise<void> => {
