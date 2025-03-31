0ffe08dc595d9dd53892ca9a0b5fac7eab0d3e65
hanseo <yds06206@gmail.com>
2025-04-01 05:22:18 +0900

    docs: story description 작성

--- packages/design-system/src/components/dropdown-menu/dropdown-menu.stories.tsx
+++ packages/design-system/src/components/dropdown-menu/dropdown-menu.stories.tsx
@@ -12,6 +12,16 @@ const meta: Meta<typeof DropdownMenu> = {
   component: DropdownMenu,
   parameters: {
     layout: 'centered',
+    docs: {
+      description: {
+        component:
+          'DropdownMenu 컴포넌트는 사용자가 선택할 수 있는 여러 항목 목록을 제공하는 컴포넌트입니다. \n\n' +
+          '- Root: 드롭다운 컨텍스트 제공, 외부 클릭 시 자동으로 닫힘 \n\n' +
+          '- Trigger: 드롭다운을 여닫는 버튼 요소 \n\n' +
+          '- Content: 드롭다운 메뉴 컨테이너 \n\n' +
+          '- Item: 개별 메뉴 항목 \n\n',
+      },
+    },
   },
   tags: ['autodocs'],
   args: {},
@@ -22,7 +32,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   render: (args) => (
-    <div>
+    <div style={{ height: '20rem' }}>
       <DropdownMenu>
         <DropdownMenu.Trigger>
           <BtnMeatball width={'2.4rem'} height={'2.4rem'} />
