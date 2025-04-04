5728d2d4b53a88165ea020e1330282dbfbbf7937
gwagjiug <kwjo0228@naver.com>
2025-04-04 21:13:46 +0900

    refactor: 헤더 컴포넌트의 내비게이션 핸들러 간소화 및 일관성 있는 클릭 핸들러 적용

--- packages/design-system/src/components/header/header.tsx
+++ packages/design-system/src/components/header/header.tsx
@@ -27,22 +27,10 @@ const Header = ({
 }: HeaderProps) => {
   const navigate = useNavigate();
 
-  const handleBackClick = (isBackToHome: boolean) => {
-    if (isBackToHome) {
-      navigate('/');
-    } else {
-      navigate(-1);
-    }
-  };
-
   const handleNavigation = (path: string) => {
     navigate(path);
     window.location.reload();
   };
-
-  const handleSearchClick = () => handleNavigation('/search');
-  const handleLogoClick = () => handleNavigation('/');
-  const handleProfileClick = () => handleNavigation('/my');
 
   if (variant === 'detail') {
     return (
@@ -51,10 +39,10 @@ const Header = ({
       >
         <button
           className={styles.button({ variant: 'back' })}
-          onClick={() => handleBackClick(isBackToHome)}
+          onClick={() => (isBackToHome ? navigate('/') : navigate(-1))}
           aria-label="뒤로가기"
         >
-          {icon ? icon : <BtnArrowLeft20 className={styles.icon} />}
+          {icon || <BtnArrowLeft20 className={styles.icon} />}
         </button>
         <h1 className={styles.title}>{title}</h1>
       </header>
@@ -63,19 +51,19 @@ const Header = ({
 
   return (
     <header className={styles.container({ variant: 'default' })}>
-      <LogoMain className={styles.logo} onClick={handleLogoClick} />
+      <LogoMain className={styles.logo} onClick={() => handleNavigation('/')} />
       <div className={styles.iconSection}>
         <button
           className={styles.button({ variant: 'default' })}
           aria-label="검색"
-          onClick={handleSearchClick}
+          onClick={() => handleNavigation('/search')}
         >
           <BtnSearchG90024 className={styles.icon} />
         </button>
         <button
           className={styles.button({ variant: 'default' })}
-          onClick={handleProfileClick}
           aria-label="프로필"
+          onClick={() => handleNavigation('/my')}
         >
           <BtnAccountGray24 className={styles.icon} />
         </button>
