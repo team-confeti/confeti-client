799ac02574248e2e7730108701e78ecf1520aaac
gwagjiug <kwjo0228@naver.com>
2025-04-08 14:05:36 +0900

    feat: Description 컴포넌트 및 스타일 추가

---
+++ packages/design-system/src/components/description/description.css.ts
@@ -0,0 +1,28 @@
+import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
+
+import { themeVars } from '../../styles';
+
+export const descriptionTextVariants = recipe({
+  base: {},
+  variants: {
+    fontSize: {
+      18: {
+        ...themeVars.fontStyles.title2_b_20,
+        color: themeVars.color.gray800,
+      },
+      20: {
+        ...themeVars.fontStyles.title3_b_18,
+        color: themeVars.color.black,
+      },
+    },
+  },
+});
+
+export const descriptioncontainer = style({
+  ...themeVars.display.flexColumn,
+  width: '100%',
+  padding: '2rem 2rem 0rem 2rem',
+  gap: '1rem',
+  alignSelf: 'stretch',
+});
