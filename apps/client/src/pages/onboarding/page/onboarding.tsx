ce764089a4ee1a17e9649ffc1667a5bc4e0c9249
gwagjiug <kwjo0228@naver.com>
2025-04-08 20:32:47 +0900

    feat: Onboarding 단계 추가 및 useFunnel 훅 구현

--- apps/client/src/pages/onboarding/page/onboarding.tsx
+++ apps/client/src/pages/onboarding/page/onboarding.tsx
@@ -1,4 +1,6 @@
 import { Avatar, Button, Description, SearchBar } from '@confeti/design-system';
+import { routePath } from '@shared/constants/path';
+import { useFunnel } from '@shared/utils/use-funnel';
 
 import * as styles from './onboarding.css';
 
@@ -9,34 +11,42 @@ const mockArtists = Array.from({ length: 100 }, (_, i) => ({
 }));
 
 const Onboarding = () => {
+  const TOTAL_STEPS = 2;
+  const { Funnel, Step, setStep } = useFunnel(TOTAL_STEPS, routePath.ROOT);
+
   return (
-    <>
-      <section className={styles.onboardingContentSection}>
-        <Description
-          descriptionText={['선호하는 아티스트를', '모두 선택해주세요']}
-          fontSize={20}
-        />
-        <div className={styles.searchBarSection}>
-          <SearchBar
-            showBackButton={false}
-            placeholder="아티스트를 검색해주세요"
+    <Funnel>
+      <Step name="1">
+        <section className={styles.onboardingContentSection}>
+          <Description
+            descriptionText={['선호하는 아티스트를', '모두 선택해주세요']}
+            fontSize={20}
           />
-        </div>
-        <div className={styles.avatarGridSection}>
-          {mockArtists.map((artist) => (
-            <div key={artist.id} className={styles.avatar}>
-              <Avatar
-                size="xl"
-                src={artist.src}
-                alt={`${artist.name} 이미지`}
-              />
-              <p className={styles.artistName}>{artist.name}</p>
-            </div>
-          ))}
-        </div>
-        <Button text={'다음'} variant={'add'} />
-      </section>
-    </>
+          <div className={styles.searchBarSection}>
+            <SearchBar
+              showBackButton={false}
+              placeholder="아티스트를 검색해주세요"
+            />
+          </div>
+          <div className={styles.avatarGridSection}>
+            {mockArtists.map((artist) => (
+              <div key={artist.id} className={styles.avatar}>
+                <Avatar
+                  size="xl"
+                  src={artist.src}
+                  alt={`${artist.name} 이미지`}
+                />
+                <p className={styles.artistName}>{artist.name}</p>
+              </div>
+            ))}
+          </div>
+          <Button text={'다음'} variant={'add'} onClick={() => setStep(1)} />
+        </section>
+      </Step>
+      <Step name="2">
+        <p>2단계</p>
+      </Step>
+    </Funnel>
   );
 };
 
