4081f638ff1f526bc34b7ba9716d0e61fe376d03
gwagjiug <kwjo0228@naver.com>
2025-04-10 02:13:36 +0900

    feat: 온보딩 페이지에서 아티스트 선택 컴포넌트 추가 및 불필요한 코드 제거

--- apps/client/src/pages/onboarding/page/onboarding.tsx
+++ apps/client/src/pages/onboarding/page/onboarding.tsx
@@ -1,14 +1,9 @@
-import { Avatar, Button, Description, SearchBar } from '@confeti/design-system';
+import { Button } from '@confeti/design-system';
 import { routePath } from '@shared/constants/path';
 import { useFunnel } from '@shared/utils/use-funnel';
 
-import * as styles from './onboarding.css';
-
-const mockArtists = Array.from({ length: 100 }, (_, i) => ({
-  id: i,
-  name: `오아시스 ${i + 1}`,
-  src: 'https://i.scdn.co/image/ab6761610000f1786a50f39b95ce98a0e6bf5b21',
-}));
+import ArtistSelect from '../components/artist-select';
+import OnBoardingComplete from '../components/onboarding-complete';
 
 const Onboarding = () => {
   const TOTAL_STEPS = 2;
@@ -17,34 +12,18 @@ const Onboarding = () => {
   return (
     <Funnel>
       <Step name="1">
-        <section className={styles.onboardingContentSection}>
-          <Description
-            descriptionText={['선호하는 아티스트를', '모두 선택해주세요']}
-            fontSize={20}
-          />
-          <div className={styles.searchBarSection}>
-            <SearchBar
-              showBackButton={false}
-              placeholder="아티스트를 검색해주세요"
-            />
-          </div>
-          <div className={styles.avatarGridSection}>
-            {mockArtists.map((artist) => (
-              <div key={artist.id} className={styles.avatar}>
-                <Avatar
-                  size="xl"
-                  src={artist.src}
-                  alt={`${artist.name} 이미지`}
-                />
-                <p className={styles.artistName}>{artist.name}</p>
-              </div>
-            ))}
-          </div>
+        <ArtistSelect>
           <Button text={'다음'} variant={'add'} onClick={() => setStep(1)} />
-        </section>
+        </ArtistSelect>
       </Step>
       <Step name="2">
-        <p>2단계</p>
+        <OnBoardingComplete>
+          <Button
+            text={'시작하기'}
+            variant={'add'}
+            onClick={() => setStep(2)}
+          />
+        </OnBoardingComplete>
       </Step>
     </Funnel>
   );
