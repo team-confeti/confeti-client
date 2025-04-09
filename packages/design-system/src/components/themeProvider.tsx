9a0ad1fa80b528379ef840c4cb7b946058dbd9ea
hanseo <yds06206@gmail.com>
2025-04-10 02:21:01 +0900

    fix: createGlobalTheme 삭제하고 기존 방식으로 롤백 후 모달 div에 theme 클래스 적용

--- packages/design-system/src/components/themeProvider.tsx
+++ packages/design-system/src/components/themeProvider.tsx
@@ -1,12 +1,19 @@
+import { ReactNode } from 'react';
+
+import { themeClass } from '../styles';
+
 import '../styles/reset.css';
 
 export default function ThemeProvider({
+  theme,
   className,
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
   theme?: string;
   className?: string;
 }) {
-  return <div className={className}>{children}</div>;
+  return (
+    <div className={`${theme ?? themeClass} ${className}`}>{children}</div>
+  );
 }
