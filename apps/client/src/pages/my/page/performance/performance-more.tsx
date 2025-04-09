7528df767afb9b2e18a5167f51f86cda4159ca3a
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-04-10 01:03:24 +0900

    feat: 필터링된 공연 목록을 위한 useFilteredPerformances 훅 추가 및 UI 개선

--- apps/client/src/pages/my/page/performance/performance-more.tsx
+++ apps/client/src/pages/my/page/performance/performance-more.tsx
@@ -1,34 +1,38 @@
+import PerformanceList from '@pages/my/components/performance/performance-list';
+import { useFilteredPerformances } from '@pages/my/hooks/use-filter-performances';
 import { useMyConfeti } from '@pages/my/hooks/use-my-favorites';
 
-import { FestivalCard, Header } from '@confeti/design-system';
-import { PERFORMANCE_DATA } from '@shared/mocks/performance-data';
+import { Chip, Header } from '@confeti/design-system';
 
 import * as styles from './performance-more.css';
 
 const ConfetiMore = () => {
   const { data } = useMyConfeti();
+  const categories = ['전체', '콘서트', '페스티벌'] as const;
 
-  if (!data) return null;
+  const { selectedCategory, setSelectedCategory, filteredPerformances } =
+    useFilteredPerformances(data?.performances || []);
 
-  const allPerformances = [
-    ...data.performances,
-    ...PERFORMANCE_DATA.performances,
-  ];
+  console.log(data?.performances);
 
   return (
     <>
       <Header variant="detail" title="My Confeti" />
-      <div className={styles.container}>
-        {allPerformances.map((performance) => (
-          <FestivalCard
-            key={performance.index}
-            typeId={performance.typeId}
-            type={performance.type}
-            title={performance.title}
-            imageSrc={performance.posterUrl}
-          />
-        ))}
-      </div>
+      <nav>
+        <ul className={styles.chipList}>
+          {categories.map((category) => (
+            <li key={category}>
+              <Chip
+                label={category}
+                variant={selectedCategory === category ? 'active' : 'default'}
+                onClick={() => setSelectedCategory(category)}
+              />
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <PerformanceList performances={filteredPerformances} />
     </>
   );
 };
