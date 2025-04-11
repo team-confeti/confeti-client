453a1b4c857e7f0f23a7bccdef503cae74c88d41
hanseo <yds06206@gmail.com>
2025-04-11 22:02:38 +0900

    feat: 회원가입 탈퇴 쿼리 훅 작성

---
+++ apps/client/src/pages/my/hooks/use-delete-account.ts
@@ -0,0 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { deleteAccount } from '@shared/apis/auth/auth';
+import { USER_QUERY_KEY } from '@shared/apis/user/user-queries';
+import { routePath } from '@shared/constants/path';
+import { authTokenHandler } from '@shared/utils/token-handler';
+
+export const useDeleteAccountMutation = () => {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteAccount,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [...USER_QUERY_KEY.PROFILE()],
+      });
+
+      authTokenHandler('remove');
+      navigate(`${routePath.ROOT}`);
+    },
+  });
+};
--- apps/client/src/shared/apis/auth/auth.ts
+++ apps/client/src/shared/apis/auth/auth.ts
@@ -2,7 +2,7 @@ import { END_POINT } from '@shared/constants/api';
 import { BaseResponse } from '@shared/types/api';
 import { KakaoLogin, SocialLoginResponse } from '@shared/types/login-response';
 
-import { axiosPublicInstance, post } from '../config/instance';
+import { axiosPublicInstance, del, post } from '../config/instance';
 
 export const postSocialLogin = async (socialLoginResponse: KakaoLogin) => {
   const response = await axiosPublicInstance.post<
@@ -13,5 +13,10 @@ export const postSocialLogin = async (socialLoginResponse: KakaoLogin) => {
 
 export const postLogout = async (): Promise<BaseResponse<void>> => {
   const response = await post<BaseResponse<void>>(END_POINT.POST_LOGOUT);
+  return response;
+};
+
+export const deleteAccount = async () => {
+  const response = await del(END_POINT.DELETE_ACCOUNT);
   return response;
 };
