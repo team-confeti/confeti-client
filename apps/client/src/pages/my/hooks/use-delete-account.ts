b72e2a8276271d148cba1660b971022da6482f9c
hanseo <yds06206@gmail.com>
2025-04-12 02:35:49 +0900

    refactor: import문 삭제

--- apps/client/src/pages/my/hooks/use-delete-account.ts
+++ apps/client/src/pages/my/hooks/use-delete-account.ts
@@ -2,7 +2,6 @@ import { useNavigate } from 'react-router-dom';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { deleteAccount } from '@shared/apis/auth/auth';
-import { USER_QUERY_KEY } from '@shared/apis/user/user-queries';
 import { routePath } from '@shared/constants/path';
 import { authTokenHandler } from '@shared/utils/token-handler';
 
