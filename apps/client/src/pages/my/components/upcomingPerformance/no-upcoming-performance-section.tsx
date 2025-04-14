bb0467e83ff186de688ccac555bb4e97ce981f6b
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-04-14 22:47:22 +0900

    feat: NoUpcomingPerformanceSection 컴포넌트 추가

---
+++ apps/client/src/pages/my/components/upcomingPerformance/no-upcoming-performance-section.tsx
@@ -0,0 +1,7 @@
+import * as styles from './no-upcoming-performance-section.css';
+
+const NoUpcomingPerformanceSection = () => {
+  return <div className={styles.wrapper}>관람 예정 공연이 없어요</div>;
+};
+
+export default NoUpcomingPerformanceSection;
