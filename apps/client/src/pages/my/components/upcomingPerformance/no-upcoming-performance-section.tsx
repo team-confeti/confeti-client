cddc4ba25b3c85bcdeafe6773fbe5f96cf54c74e
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-04-14 22:54:37 +0900

    fix: NoUpcomingPerformanceSection 컴포넌트의 텍스트를 p 태그로 감싸기

--- apps/client/src/pages/my/components/upcomingPerformance/no-upcoming-performance-section.tsx
+++ apps/client/src/pages/my/components/upcomingPerformance/no-upcoming-performance-section.tsx
@@ -1,7 +1,11 @@
 import * as styles from './no-upcoming-performance-section.css';
 
 const NoUpcomingPerformanceSection = () => {
-  return <div className={styles.wrapper}>관람 예정 공연이 없어요</div>;
+  return (
+    <div className={styles.wrapper}>
+      <p>관람 예정 공연이 없어요</p>
+    </div>
+  );
 };
 
 export default NoUpcomingPerformanceSection;
