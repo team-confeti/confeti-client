e0418c369d109a325a274f327f44b1740575e1af
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-04-15 16:27:31 +0900

    feat: 내 공연 탭 추가 및 네비게이션 컴포넌트 추가

---
+++ apps/client/src/shared/components/navigation-tabs.tsx
@@ -0,0 +1,51 @@
+import { useNavigate } from 'react-router-dom';
+import { TAB_MENU } from '@pages/home/constants/menu';
+
+import { Navigation } from '@confeti/design-system';
+import { routePath } from '@shared/constants/path';
+
+interface Props {
+  defaultActiveTab: TAB_MENU;
+  hidden?: boolean;
+}
+
+const NavigationTabs = ({ defaultActiveTab, hidden = false }: Props) => {
+  const navigate = useNavigate();
+
+  if (hidden) {
+    return null;
+  }
+
+  const handleNavigation = (path: string) => {
+    navigate(path);
+  };
+
+  console.log(TAB_MENU.HOME);
+
+  return (
+    <Navigation.Root defaultActiveTab={defaultActiveTab}>
+      <Navigation.List>
+        <Navigation.Item
+          index={TAB_MENU.HOME}
+          handleTabClick={() => handleNavigation(routePath.ROOT)}
+        >
+          {'홈'}
+        </Navigation.Item>
+        <Navigation.Item
+          index={TAB_MENU.TIMETABLE}
+          handleTabClick={() => handleNavigation(routePath.TIME_TABLE_OUTLET)}
+        >
+          {'타임테이블'}
+        </Navigation.Item>
+        <Navigation.Item
+          index={TAB_MENU.MY_HISTORY}
+          handleTabClick={() => handleNavigation(routePath.MY_HISTORY)}
+        >
+          {'내 공연'}
+        </Navigation.Item>
+      </Navigation.List>
+    </Navigation.Root>
+  );
+};
+
+export default NavigationTabs;
