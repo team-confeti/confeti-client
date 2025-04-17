25e4a66c4dc554a541fc4cf83bc990b10bb38c23
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-04-18 00:09:12 +0900

    Feat(client): My confeti 전체 목록 조회 API 연동 (#381)
    * refactor: 사용자 아티스트 및 공연 미리보기 API 커스텀 훅 및 쿼리옵션 네이밍 변경
    
    * feat: MyPerformances 및 MyPerformancesResponse 인터페이스 추가
    
    * feat: MyPerformances API 및 관련 훅 추가

--- apps/client/src/pages/my/hooks/use-my-favorites.ts
+++ apps/client/src/pages/my/hooks/use-my-favorites.ts
@@ -1,24 +1,33 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { USER_QUERY_OPTIONS } from '@shared/apis/user/user-queries';
+import { PerformancesFilterType } from '@shared/types/user-response';
 import { checkIsNotLoggedIn } from '@shared/utils/check-is-not-logged-in';
 
-export const useMyArtist = () => {
+export const useMyArtistPreview = () => {
   const isNotLoggedIn = checkIsNotLoggedIn();
   const { data } = useQuery({
-    ...USER_QUERY_OPTIONS.FAVORITE_ARTISTS(),
+    ...USER_QUERY_OPTIONS.MY_ARTISTS_PREVIEW(),
     enabled: !isNotLoggedIn,
   });
   // 로딩 상태 제거
   return { data };
 };
 
-export const useMyConfeti = () => {
+export const useMyPerformancePreview = () => {
   const isNotLoggedIn = checkIsNotLoggedIn();
   const { data } = useQuery({
-    ...USER_QUERY_OPTIONS.FAVORITE_PERFORMANCES(),
+    ...USER_QUERY_OPTIONS.MY_PERFORMANCES_PREVIEW(),
     enabled: !isNotLoggedIn,
   });
   // 로딩 상태 제거
+  return { data };
+};
+
+export const useMyPerformances = (performancesType: PerformancesFilterType) => {
+  const { data } = useQuery({
+    ...USER_QUERY_OPTIONS.MY_PERFORMANCES(performancesType),
+  });
+
   return { data };
 };
--- apps/client/src/pages/my/page/profile/my-profile.tsx
+++ apps/client/src/pages/my/page/profile/my-profile.tsx
@@ -6,7 +6,7 @@ import LogoutSection from '@pages/my/components/profile/logout-section';
 import UserInfo from '@pages/my/components/profile/user-info';
 import NoUpcomingPerformanceSection from '@pages/my/components/upcomingPerformance/no-upcoming-performance-section';
 import UpcomingPerformanceSection from '@pages/my/components/upcomingPerformance/upcoming-performance-section';
-import { useMyConfeti } from '@pages/my/hooks/use-my-favorites';
+import { useMyPerformancePreview } from '@pages/my/hooks/use-my-favorites';
 import { useUserProfile } from '@pages/my/hooks/use-user-info';
 
 import { Box, Footer, Header } from '@confeti/design-system';
@@ -21,7 +21,7 @@ const MyProfile = () => {
   // const { data: artistData } = useMyArtist();
   const artistData = ARTISTS_DATA;
   const upcomingPerformanceData = PERFORMANCE_DATA.performances[0];
-  const { data: performanceData } = useMyConfeti();
+  const { data: performanceData } = useMyPerformancePreview();
 
   if (!profileData || !artistData || !performanceData) {
     return null;
