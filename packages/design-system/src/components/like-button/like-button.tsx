79e42e3c9523ab790787e0e4edb6efd1fa27936d
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-04-21 00:22:58 +0900

    refactor: 불필요한 useEffect 제거

--- packages/design-system/src/components/like-button/like-button.tsx
+++ packages/design-system/src/components/like-button/like-button.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { toast } from '@confeti/design-system';
 import { BtnHeart } from '@confeti/design-system/icons';
