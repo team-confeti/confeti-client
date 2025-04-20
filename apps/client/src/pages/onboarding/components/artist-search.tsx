65825a920efd1adc223628b2a1ad58bff72a403b
gwagjiug <kwjo0228@naver.com>
2025-04-20 16:41:50 +0900

    feat: 아티스트 검색 기능 개선 및 검색 제안 목록 추가

--- apps/client/src/pages/onboarding/components/artist-search.tsx
+++ apps/client/src/pages/onboarding/components/artist-search.tsx
@@ -1,25 +1,65 @@
-import { SearchBar } from '@confeti/design-system';
+import { useCallback, useEffect, useState } from 'react';
+import { debounce } from 'lodash';
+
+import { SearchBar, SearchSuggestionList } from '@confeti/design-system';
+
+import { useArtistRelatedKeyword } from '../hooks/use-get-related-keyword';
 
 import * as styles from './artist-search.css';
 
-// interface ArtistSearchProps {
-//   handleArtistSelect: () => void;
-// }
-
-//TODO : 검색결과 유무로 조건부 렌더링 로직 추가 및 아티스트 리스트 클릭 시 handleArtistSelect onClick전달
 const ArtistSearch = () => {
+  const [keyword, setKeyword] = useState('');
+  const [debouncedKeyword, setDebouncedKeyword] = useState('');
+
+  const relatedKeywordsData = useArtistRelatedKeyword({
+    keyword: debouncedKeyword,
+    enabled: !!debouncedKeyword.trim(),
+  });
+
+  const debounceInput = useCallback(
+    debounce((nextValue: string) => {
+      setDebouncedKeyword(nextValue);
+    }, 500),
+    [],
+  );
+
+  const handleSearchBarChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    setKeyword(inputValue);
+    debounceInput(inputValue);
+  };
+
+  useEffect(() => {
+    return () => {
+      debounceInput.cancel();
+    };
+  }, [debounceInput]);
+
+  const hasArtistResults = (relatedKeywordsData?.artists?.length ?? 0) > 0;
+
   return (
     <>
       <div className={styles.searchBarContainer}>
         <div className={styles.searchBarFrame}>
-          <SearchBar placeholder="아티스트 또는 공연을 검색해보세요!" />
+          <SearchBar
+            placeholder="아티스트 또는 공연을 검색해보세요!"
+            value={keyword}
+            onChange={handleSearchBarChange}
+          />
         </div>
       </div>
-      <section className={styles.artistSearchContainer}>
-        <p className={styles.artistSearchDescription}>
-          선호하는 아티스트를 검색해보세요
-        </p>
-      </section>
+
+      {hasArtistResults ? (
+        <section className={styles.searchSuggestionListSection}>
+          <SearchSuggestionList relatedKeyword={relatedKeywordsData?.artists} />
+        </section>
+      ) : (
+        <section className={styles.artistSearchContainer}>
+          <p className={styles.artistSearchDescription}>
+            선호하는 아티스트를 검색해보세요
+          </p>
+        </section>
+      )}
     </>
   );
 };
