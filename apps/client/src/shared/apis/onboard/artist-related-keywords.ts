1efa805621b88d84a783bb37c6e28eb885dc7ef5
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-04-21 15:09:02 +0900

    refactor: axiosPublicInstance를 get 함수로 변경하여 API 호출 방식 통일

--- apps/client/src/shared/apis/onboard/artist-related-keywords.ts
+++ apps/client/src/shared/apis/onboard/artist-related-keywords.ts
@@ -2,13 +2,13 @@ import { END_POINT } from '@shared/constants/api';
 import { BaseResponse } from '@shared/types/api';
 import { onboardResponse } from '@shared/types/onboard-response';
 
-import { axiosInstance } from '../config/instance';
+import { get } from '../config/instance';
 
 export const getArtistRelatedKeyword = async (
   keyword: string,
 ): Promise<onboardResponse> => {
-  const response = await axiosInstance.get<BaseResponse<onboardResponse>>(
+  const response = await get<BaseResponse<onboardResponse>>(
     END_POINT.GET_ARTIST_RELATED_KEYWORDS(keyword, 10),
   );
-  return response.data.data;
+  return response.data;
 };
