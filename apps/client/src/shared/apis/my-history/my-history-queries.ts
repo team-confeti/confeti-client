e0bf9777826eee97a87e9a4b6623a5bbc3f3a120
gwagjiug <kwjo0228@naver.com>
2025-04-24 02:48:03 +0900

    feat: 내 기록 API에 타임테이블 개요 조회 기능 추가 및 정렬 옵션 적용

--- apps/client/src/shared/apis/my-history/my-history-queries.ts
+++ apps/client/src/shared/apis/my-history/my-history-queries.ts
@@ -1,21 +1,31 @@
 import { queryOptions } from '@tanstack/react-query';
 
-import { getMyTimeTablePreview } from './my-history';
+import { SortOption } from '@shared/constants/sort-label';
 
-export const MY_HISTORY_TIME_TABLE_PREVIEW_QUERY_KEY = {
-  ALL: ['time-table-preview'],
+import { getMyTimeTableOverView, getMyTimeTablePreview } from './my-history';
+
+export const MY_HISTORY_TIME_TABLE_QUERY_KEY = {
+  ALL: ['my-history-timetable'],
   MY_TIME_TABLE_PREVIEW: () => [
-    ...MY_HISTORY_TIME_TABLE_PREVIEW_QUERY_KEY.ALL,
-    'time-table-preview',
+    ...MY_HISTORY_TIME_TABLE_QUERY_KEY.ALL,
+    'preview',
+  ],
+  MY_TIME_TABLE_OVERVIEW: (sortBy: SortOption) => [
+    ...MY_HISTORY_TIME_TABLE_QUERY_KEY.ALL,
+    'overview',
+    sortBy,
   ],
 } as const;
 
-export const MY_HISTORY_TIME_TABLE_PREVIEW_QUERY_OPTION = {
-  ALL: () =>
-    queryOptions({ queryKey: MY_HISTORY_TIME_TABLE_PREVIEW_QUERY_KEY.ALL }),
-  MY_TIME_TABLE_PREVIEW: () =>
+export const MY_HISTORY_TIME_TABLE_QUERY_OPTION = {
+  ALL: () => queryOptions({ queryKey: MY_HISTORY_TIME_TABLE_QUERY_KEY.ALL }),
+  PREVIEW: () =>
     queryOptions({
-      queryKey: MY_HISTORY_TIME_TABLE_PREVIEW_QUERY_KEY.MY_TIME_TABLE_PREVIEW(),
+      queryKey: MY_HISTORY_TIME_TABLE_QUERY_KEY.MY_TIME_TABLE_PREVIEW(),
       queryFn: getMyTimeTablePreview,
     }),
+  OVERVIEW: (sortBy: SortOption) => ({
+    queryKey: MY_HISTORY_TIME_TABLE_QUERY_KEY.MY_TIME_TABLE_OVERVIEW(sortBy),
+    queryFn: () => getMyTimeTableOverView(sortBy),
+  }),
 } as const;
--- apps/client/src/shared/apis/my-history/my-history.ts
+++ apps/client/src/shared/apis/my-history/my-history.ts
@@ -1,4 +1,5 @@
 import { END_POINT } from '@shared/constants/api';
+import { SortOption } from '@shared/constants/sort-label';
 import { BaseResponse } from '@shared/types/api';
 import { MyHistoryResponse } from '@shared/types/my-history-response';
 
@@ -7,6 +8,13 @@ import { get } from '../config/instance';
 export const getMyTimeTablePreview = async () => {
   const response = await get<BaseResponse<MyHistoryResponse>>(
     END_POINT.GET_MY_TIMETABLE,
+  );
+  return response.data;
+};
+
+export const getMyTimeTableOverView = async (sortBy: SortOption) => {
+  const response = await get<BaseResponse<MyHistoryResponse>>(
+    END_POINT.GET_MY_TIMETABLE_OVERVIEW(sortBy),
   );
   return response.data;
 };
