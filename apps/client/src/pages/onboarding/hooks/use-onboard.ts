53c806f2ea2e402d501629b62be3cff0e36ff561
gwagjiug <kwjo0228@naver.com>
2025-04-23 18:12:14 +0900

    feat: 온보딩 관련 커스텀 훅의 주석 제거

--- apps/client/src/pages/onboarding/hooks/use-onboard.ts
+++ apps/client/src/pages/onboarding/hooks/use-onboard.ts
@@ -13,15 +13,6 @@ interface UseArtistRelatedKeywordProps {
   limit: number;
 }
 
-/**
- * 서버에서 인기 아티스트 목록을 가져오는 커스텀 훅
- *
- * 이 훅은 온보딩 화면에서 처음 진입할 때 사용됩니다.
- * 인기 아티스트 리스트를 서버에서 요청하여 반환합니다.
- *
- * @param limit - 가져올 아티스트의 최대 개수
- * @returns { data: onboard[] } - 인기 아티스트 목록 데이터
- */
 export const useGetTopArtist = (limit: number) => {
   const { data } = useSuspenseQuery({
     ...TOP_ARTIST_QUERY_OPTION.TOP_ARTIST(limit),
@@ -30,17 +21,6 @@ export const useGetTopArtist = (limit: number) => {
   return { data };
 };
 
-/**
- * 아티스트 연관 검색어를 가져오는 커스텀 훅
- *
- * 사용자가 입력한 검색 키워드에 맞춰 연관된 아티스트를 검색하여 가져옵니다.
- * 이 훅은 사용자가 입력한 검색 키워드에 따라 디바운싱 후 연관 검색어를 요청합니다.
- *
- * @param keyword - 사용자가 입력한 검색 키워드
- * @param enabled - 검색어가 있을 때만 쿼리를 실행하도록 설정하는 옵션
- * @param limit - 연관 검색어의 최대 개수
- * @returns { data: onboard[] | undefined } - 연관된 아티스트 데이터
- */
 export const useArtistRelatedKeyword = ({
   keyword,
   enabled,
@@ -54,15 +34,6 @@ export const useArtistRelatedKeyword = ({
   return data;
 };
 
-/**
- * 아티스트 연관 아티스트를 가져오는 커스텀 훅
- *
- * 이 훅은 기준이 되는 아티스트 ID에 맞춰 해당 아티스트와 연관된 다른 아티스트 데이터를 가져옵니다.
- * 성공적으로 데이터를 가져오면 `setArtists` 함수를 사용하여 연관 아티스트 목록을 업데이트합니다.
- *
- * @param setArtists - 연관 아티스트 목록을 업데이트하는 상태 변경 함수
- * @returns { mutate: Mutation } - 아티스트 연관 아티스트 데이터를 가져오는 Mutation 함수
- */
 export const useArtistRelatedArtist = () => {
   return useMutation<
     BaseResponse<onboardResponse>,
@@ -75,15 +46,6 @@ export const useArtistRelatedArtist = () => {
   });
 };
 
-/**
- * 온보딩 완료 후 인증된 아티스트 목록을 서버에 전송하는 커스텀 훅
- *
- * 이 훅은 사용자가 선택한 아티스트 목록을 서버에 전송하여 온보딩을 완료합니다.
- * 성공적으로 데이터를 전송하면 온보딩 상태가 갱신되며, 해당 데이터를 서버에 저장합니다.
- *
- * @param favoriteArtists - 사용자가 선택한 선호하는 아티스트 목록
- * @returns { mutate: Mutation } - 온보딩 완료 데이터를 서버에 전송하는 Mutation 함수
- */
 export const usePostAuthOnboarding = () => {
   return useMutation({
     mutationFn: (favoriteArtists: string[]) => {
