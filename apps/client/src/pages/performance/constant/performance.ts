df38c1e5727fd5fbb581d98f508fe1bfb3fb4fb5
daahyunk <ekgusdl0408@naver.com>
2025-04-24 19:30:37 +0900

    perforamce 상수 데이터 수정 및 적용

--- apps/client/src/pages/performance/constant/performance.ts
+++ apps/client/src/pages/performance/constant/performance.ts
@@ -1,11 +1,17 @@
 export const PERFORMANCE_LABEL = {
+  //section title
   PERFORMANCE_INFO: '공연 정보',
-  PERFORMANCE_NAME: '공연 이름',
+  RESERVATION_OFFICE: '예매처 바로가기',
+  DETAIL_INFO: '상세 정보',
+  TICKET_INFO: '티켓 정보',
+  LOCATION: '오시는 길',
+
+  //label
+  PERIOD: '기간',
   PLACE: '장소',
-  PERIOD: '공연 기간',
+  TICKETING_DATE: '예매일',
+  NAME: '공연 이름',
   TIME: '공연 시간',
   AGERATING: '관람 등급',
-  RESERVATIONOFFICE: '예매처',
-  TICKET_INFO: '티켓 정보',
   PRICE: '가격',
 };
