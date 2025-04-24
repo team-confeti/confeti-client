d9b7197d32d4a88150d58b723092af45a5f95d5b
gwagjiug <kwjo0228@naver.com>
2025-04-24 15:45:43 +0900

    feat: 내 공연 세트리스트 미리보기 및 타임테이블 API 연동 추가

--- apps/client/src/shared/apis/my-history/my-history-queries.ts
+++ apps/client/src/shared/apis/my-history/my-history-queries.ts
@@ -2,30 +2,62 @@ import { queryOptions } from '@tanstack/react-query';
 
 import { SortOption } from '@shared/constants/sort-label';
 
-import { getMyTimeTableOverView, getMyTimeTablePreview } from './my-history';
+import {
+  getMySetListPreview,
+  getMyTimeTableOverView,
+  getMyTimeTablePreview,
+} from './my-history';
 
-export const MY_HISTORY_TIME_TABLE_QUERY_KEY = {
-  ALL: ['my-history-timetable'],
-  MY_TIME_TABLE_PREVIEW: () => [
-    ...MY_HISTORY_TIME_TABLE_QUERY_KEY.ALL,
-    'preview',
-  ],
-  MY_TIME_TABLE_OVERVIEW: (sortBy: SortOption) => [
-    ...MY_HISTORY_TIME_TABLE_QUERY_KEY.ALL,
-    'overview',
-    sortBy,
-  ],
+export const MY_HISTORY_QUERY_KEY = {
+  ALL: ['my-history'],
+  TIME_TABLE: {
+    ALL: () => [...MY_HISTORY_QUERY_KEY.ALL, 'time-table'],
+    PREVIEW: () => [...MY_HISTORY_QUERY_KEY.ALL, 'time-table', 'preview'],
+    OVERVIEW: (sortBy: SortOption) => [
+      ...MY_HISTORY_QUERY_KEY.ALL,
+      'time-table',
+      'overview',
+      sortBy,
+    ],
+  },
+  SETLIST: {
+    ALL: () => [...MY_HISTORY_QUERY_KEY.ALL, 'setlist'],
+    PREVIEW: () => [...MY_HISTORY_QUERY_KEY.ALL, 'setlist', 'preview'],
+    OVERVIEW: (sortBy: SortOption) => [
+      ...MY_HISTORY_QUERY_KEY.ALL,
+      'setlist',
+      'overview',
+      sortBy,
+    ],
+  },
 } as const;
 
-export const MY_HISTORY_TIME_TABLE_QUERY_OPTION = {
-  ALL: () => queryOptions({ queryKey: MY_HISTORY_TIME_TABLE_QUERY_KEY.ALL }),
-  PREVIEW: () =>
-    queryOptions({
-      queryKey: MY_HISTORY_TIME_TABLE_QUERY_KEY.MY_TIME_TABLE_PREVIEW(),
-      queryFn: getMyTimeTablePreview,
-    }),
-  OVERVIEW: (sortBy: SortOption) => ({
-    queryKey: MY_HISTORY_TIME_TABLE_QUERY_KEY.MY_TIME_TABLE_OVERVIEW(sortBy),
-    queryFn: () => getMyTimeTableOverView(sortBy),
-  }),
+export const MY_HISTORY_QUERY_OPTION = {
+  TIME_TABLE: {
+    ALL: () =>
+      queryOptions({
+        queryKey: MY_HISTORY_QUERY_KEY.TIME_TABLE.ALL(),
+      }),
+    PREVIEW: () =>
+      queryOptions({
+        queryKey: MY_HISTORY_QUERY_KEY.TIME_TABLE.PREVIEW(),
+        queryFn: getMyTimeTablePreview,
+      }),
+    OVERVIEW: (sortBy: SortOption) =>
+      queryOptions({
+        queryKey: MY_HISTORY_QUERY_KEY.TIME_TABLE.OVERVIEW(sortBy),
+        queryFn: () => getMyTimeTableOverView(sortBy),
+      }),
+  },
+  SETLIST: {
+    ALL: () =>
+      queryOptions({
+        queryKey: MY_HISTORY_QUERY_KEY.SETLIST.ALL(),
+      }),
+    PREVIEW: () =>
+      queryOptions({
+        queryKey: MY_HISTORY_QUERY_KEY.SETLIST.PREVIEW(),
+        queryFn: getMySetListPreview,
+      }),
+  },
 } as const;
--- apps/client/src/shared/apis/my-history/my-history.ts
+++ apps/client/src/shared/apis/my-history/my-history.ts
@@ -1,20 +1,30 @@
 import { END_POINT } from '@shared/constants/api';
 import { SortOption } from '@shared/constants/sort-label';
 import { BaseResponse } from '@shared/types/api';
-import { MyHistoryResponse } from '@shared/types/my-history-response';
+import {
+  MyHistorySetListResponse,
+  MyHistoryTimetableResponse,
+} from '@shared/types/my-history-response';
 
 import { get } from '../config/instance';
 
 export const getMyTimeTablePreview = async () => {
-  const response = await get<BaseResponse<MyHistoryResponse>>(
+  const response = await get<BaseResponse<MyHistoryTimetableResponse>>(
     END_POINT.GET_MY_TIMETABLE,
   );
   return response.data;
 };
 
 export const getMyTimeTableOverView = async (sortBy: SortOption) => {
-  const response = await get<BaseResponse<MyHistoryResponse>>(
+  const response = await get<BaseResponse<MyHistoryTimetableResponse>>(
     END_POINT.GET_MY_TIMETABLE_OVERVIEW(sortBy),
+  );
+  return response.data;
+};
+
+export const getMySetListPreview = async () => {
+  const response = await get<BaseResponse<MyHistorySetListResponse>>(
+    END_POINT.GET_MY_SET_LIST,
   );
   return response.data;
 };
