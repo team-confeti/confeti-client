abe2860b0587d234899c59d3d09ea06de1a0ae1f
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-04-28 04:37:14 +0900

    style: 셋리스트 공연 컨테이너 스타일 수정 및 버튼 섹션 위치 조정

--- apps/client/src/pages/my-history/components/add-setlist/setlist-performance.css.ts
+++ apps/client/src/pages/my-history/components/add-setlist/setlist-performance.css.ts
@@ -5,9 +5,8 @@ import { themeVars } from '@confeti/design-system/styles';
 export const container = style({
   ...themeVars.display.flexColumn,
   width: '100%',
-  height: 'calc(100dvh - 1rem)',
+  height: 'calc(100dvh - 5rem)',
   padding: '2rem 2rem 0 2rem',
-  overflowY: 'auto',
 });
 
 export const title = style({
@@ -23,22 +22,15 @@ export const performanceContainer = style({
   gridTemplateColumns: 'repeat(3, 1fr)',
   gridColumnGap: '1.8rem',
   gridRowGap: '3rem',
-  paddingBottom: '10rem',
 });
 
 export const buttonSection = style({
   position: 'fixed',
-  bottom: '0',
-  left: '0',
-  right: '0',
-  height: '9rem',
-  padding: '0 2rem',
-
+  padding: '2rem',
+  bottom: 0,
+  left: '50%',
+  width: 'min(100%, var(--max-width))',
+  transform: 'translateX(-50%)',
   background:
     'linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #FFF 100%)',
-
-  zIndex: 100,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
 });
