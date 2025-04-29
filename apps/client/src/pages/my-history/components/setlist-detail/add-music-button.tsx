7538d0d2506750c2fb0cfe50dcdd57526e987226
daahyunk <ekgusdl0408@naver.com>
2025-04-29 21:55:54 +0900

    feat: 곡 추가 버튼 컴포넌트 구현

---
+++ apps/client/src/pages/my-history/components/setlist-detail/add-music-button.tsx
@@ -0,0 +1,16 @@
+import { IcAdd } from '@confeti/design-system/icons';
+
+import * as styles from './setlist-tracks.css';
+
+const AddMusicButton = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <button className={styles.addMusicWrapper} onClick={onClick}>
+      <div className={styles.addIconBox}>
+        <IcAdd width="30" height="30" />
+      </div>
+      <span className={styles.addText}>음악 추가하기</span>
+    </button>
+  );
+};
+
+export default AddMusicButton;
