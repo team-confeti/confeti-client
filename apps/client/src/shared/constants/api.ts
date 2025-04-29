e58916bb46574d3bea927cccd1041375f9b89f9e
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-04-29 19:31:54 +0900

    feat: 애플 로그인 리다이렉트 URI 추가

--- apps/client/src/shared/constants/api.ts
+++ apps/client/src/shared/constants/api.ts
@@ -5,6 +5,7 @@ export const CONFIG = {
   KAKAO_REDIRECT_URI: import.meta.env.VITE_KAKAO_REDIRECT_URI as string,
   KAKAO_URI: import.meta.env.VITE_KAKAO_URI as string,
   APPLE_CLIENT_ID: import.meta.env.VITE_APPLE_CLIENT_ID as string,
+  APPLE_REDIRECT_URI: import.meta.env.VITE_APPLE_REDIRECT_URI as string,
   AMPLITUDE_API_KEY: import.meta.env.VITE_AMPLITUDE_API_KEY as string,
   SENTRY_DSN: import.meta.env.VITE_SENTRY_DSN as string,
 } as const;
