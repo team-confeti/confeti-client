f189c6443397650d035dc1270988e8d8510fbe1e
gwagjiug <kwjo0228@naver.com>
2025-05-08 23:02:48 +0900

    fix: 티켓 정보 컴포넌트의 레이아웃 및 스타일 개선

--- apps/client/src/pages/home/components/ticketing-info.tsx
+++ apps/client/src/pages/home/components/ticketing-info.tsx
@@ -3,19 +3,21 @@ import * as styles from './ticketing-info.css';
 const TicketingInfo = ({ userName }: { userName: string | null }) => {
   return (
     <div>
-      <p className={styles.ticketingBannerText}>
+      <div className={styles.ticketingBannerText}>
         {userName ? (
           <>
-            <span className={styles.highlightedText}>{userName}</span>
-            님 <br />
-            예매가 다가오고 있어요!
+            <div>
+              <span className={styles.highlightedText}>{userName}</span>님
+            </div>
+            <p>예매가 다가오고 있어요!</p>
           </>
         ) : (
           <>
-            공연의 시작과 끝을 <br /> 콘페티와 함께해보세요!
+            <p>공연의 시작과 끝을</p>
+            <p>콘페티와 함께해보세요!</p>
           </>
         )}
-      </p>
+      </div>
     </div>
   );
 };
