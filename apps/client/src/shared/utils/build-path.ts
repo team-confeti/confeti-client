c6c75a6f2aef1862b17c3d7d49fef30916e427c9
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-05-09 01:00:10 +0900

    refactor: buildPath 함수 내에서 number를  params로 받을 수 있도록 변경

--- apps/client/src/shared/utils/build-path.ts
+++ apps/client/src/shared/utils/build-path.ts
@@ -1,9 +1,18 @@
-import { generatePath } from 'react-router-dom';
+import { generatePath, type PathParam } from 'react-router-dom';
 
 import { type Routes } from '@shared/router/path';
 
-export function buildPath<P extends Routes>(
-  ...args: Parameters<typeof generatePath<P>>
-): string {
-  return generatePath(...args);
-}
+export const buildPath = <P extends Routes>(
+  path: P,
+  params: { [key in PathParam<P>]: string | number },
+): string => {
+  const convertedParams = Object.entries(params).reduce(
+    (acc, [key, value]) => {
+      acc[key as PathParam<P>] = String(value);
+      return acc;
+    },
+    {} as { [key in PathParam<P>]: string },
+  );
+
+  return generatePath(path, convertedParams);
+};
