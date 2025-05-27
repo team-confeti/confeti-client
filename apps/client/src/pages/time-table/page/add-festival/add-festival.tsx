1f1f38fd022dc01fc8665028558d8f9712737606
곽지욱 <99489686+gwagjiug@users.noreply.github.com>
2025-05-27 20:56:52 +0900

    Fix(client): 페스티벌 추가 갯수 수정 및 toast 이벤트 수정 (#528)
    * fix: 최대 선택 수를 5로 변경
    
    * fix: 추가하기 버튼의 활성화 조건 수정 및 최대 선택 수 체크 로직 개선
    
    * fix: 추가하기 버튼의 비활성화 로직을 개선하여 가독성 향상

--- apps/client/src/pages/time-table/page/add-festival/add-festival.tsx
+++ apps/client/src/pages/time-table/page/add-festival/add-festival.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAddTimeTableFestival } from '@pages/time-table/hooks/use-timetable-festival-mutation';
 
@@ -23,13 +24,17 @@ const AddFestival = () => {
     navigate(routePath.TIME_TABLE_OUTLET);
   });
   const TOTAL_SELECTIONS = selectedFestivals.length + addedFestivals.length;
+  const isButtonDisabled =
+    selectedFestivals.length === 0 || TOTAL_SELECTIONS >= MAX_SELECTIONS;
+
+  useEffect(() => {
+    if (TOTAL_SELECTIONS >= MAX_SELECTIONS) {
+      showToast();
+    }
+  }, [selectedFestivals, TOTAL_SELECTIONS, showToast]);
 
   const handleAddClick = () => {
-    if (TOTAL_SELECTIONS > MAX_SELECTIONS) {
-      showToast();
-    } else {
-      addFestival(selectedFestivals);
-    }
+    addFestival(selectedFestivals);
   };
 
   return (
@@ -64,7 +69,7 @@ const AddFestival = () => {
         <Button
           variant="add"
           text={'추가하기'}
-          disabled={selectedFestivals.length === 0}
+          disabled={isButtonDisabled}
           onClick={handleAddClick}
         />
       </div>
