e27a32fa0b1e8d5d60a9389472acf8ec0a4b76a5
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-05-29 19:30:46 +0900

    feat: 소셜 로그인 뮤테이션으로 로그인 통합

--- apps/client/src/pages/auth/hooks/use-social-login-mutation.ts
+++ apps/client/src/pages/auth/hooks/use-social-login-mutation.ts
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { postSocialLogin } from '@shared/apis/auth/auth';
 import { routePath } from '@shared/router/path';
@@ -11,40 +11,24 @@ import {
 } from '@shared/types/login-response';
 import { authTokenHandler } from '@shared/utils/token-handler';
 
+type SocialLogin = KakaoLogin | AppleLogin;
+
 export const useSocialLoginMutation = () => {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
-  return useMutation<BaseResponse<SocialLoginResponse>, Error, KakaoLogin>({
+  return useMutation<BaseResponse<SocialLoginResponse>, Error, SocialLogin>({
     mutationFn: (socialLoginData) => postSocialLogin(socialLoginData),
-    onSuccess: (data) => {
+    onSuccess: async (data) => {
       if (data?.data) {
         const { accessToken, refreshToken, isOnboarding } = data.data;
         authTokenHandler('set', accessToken, refreshToken);
+        await queryClient.invalidateQueries();
+
         navigate(
           isOnboarding ? `${routePath.ONBOARDING}` : `${routePath.ROOT}`,
         );
       }
-      // TODO: 토큰 발급 후 새로고침 필요
-      window.location.reload();
-    },
-  });
-};
-
-export const useAppleLoginMutation = () => {
-  const navigate = useNavigate();
-
-  return useMutation<BaseResponse<SocialLoginResponse>, Error, AppleLogin>({
-    mutationFn: (socialLoginData) => postSocialLogin(socialLoginData),
-    onSuccess: (data) => {
-      if (data?.data) {
-        const { accessToken, refreshToken, isOnboarding } = data.data;
-        authTokenHandler('set', accessToken, refreshToken);
-        navigate(
-          isOnboarding ? `${routePath.ONBOARDING}` : `${routePath.ROOT}`,
-        );
-      }
-      // TODO: 토큰 발급 후 새로고침 필요
-      window.location.reload();
     },
   });
 };
