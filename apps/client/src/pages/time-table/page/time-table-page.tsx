ea3174a1a88e24979fc0c0884c9517064c4e2d91
gwagjiug <kwjo0228@naver.com>
2025-05-30 11:49:49 +0900

    feat: 타임테이블 페이지에 로드된 상태 컴포넌트 추가 및 스타일 정의

--- apps/client/src/pages/time-table/page/time-table-page.tsx
+++ apps/client/src/pages/time-table/page/time-table-page.tsx
@@ -1,12 +1,5 @@
 import { useMemo } from 'react';
-import Calender from '@pages/time-table/components/calender/calender';
-import FestivalSelector from '@pages/time-table/components/festival-selector/festival-selector';
-import FestivalStage from '@pages/time-table/components/festival-stage/festival-stage';
-import TimeTableActions from '@pages/time-table/components/time-table-actions/time-table-actions';
-import TimeTableBoard from '@pages/time-table/components/time-table-board/time-table-board';
 import { useFestivalSelect } from '@pages/time-table/hooks/use-festival-select';
-import { useImageDownload } from '@pages/time-table/hooks/use-image-download';
-import { useTimeTableEdit } from '@pages/time-table/hooks/use-time-table-edit';
 import EmptyFestivalSection from '@pages/time-table/page/empty/empty-festival-section';
 
 import { SwitchCase } from '@shared/components/switch-case';
@@ -16,27 +9,14 @@ import {
   useFestivalTimetableData,
   useTimeTableCreationHistory,
 } from '../hooks/use-festival-data';
+import TimeTableLoaded from './loaded/time-table-loaded';
 import TimeTableOnboard from './onboading/time-table-onboard-page';
 
-import * as styles from './time-table-page.css';
-
 const TimeTablePage = () => {
-  const { isEditTimeTableMode, toggleEditTimeTableMode } = useTimeTableEdit();
   const { festivals } = useFestivalButtonData();
   const { hasTimetableHistory } = useTimeTableCreationHistory();
-
-  const {
-    selectedFestivalInfo,
-    selectedDateId,
-    handleSelectFestival,
-    handleSelectDate,
-  } = useFestivalSelect(festivals);
-
+  const { selectedDateId } = useFestivalSelect(festivals);
   const { data: boardData } = useFestivalTimetableData(selectedDateId);
-
-  const { elementRef, downloadImage } = useImageDownload<HTMLDivElement>({
-    fileName: `${selectedFestivalInfo.title}`,
-  });
 
   const timetableState = useMemo<'onboard' | 'empty' | 'loaded'>(() => {
     if (!hasTimetableHistory) return 'onboard';
@@ -53,29 +33,7 @@ const TimeTablePage = () => {
         empty: () => <EmptyFestivalSection />,
         loaded: () =>
           boardData ? (
-            <div className={styles.wrapper}>
-              <FestivalSelector
-                festivals={festivals}
-                selectedFestivalId={selectedFestivalInfo.festivalId}
-                handleSelectFestival={handleSelectFestival}
-              />
-              <Calender
-                festivalDates={selectedFestivalInfo.festivalDates}
-                onDateSelect={handleSelectDate}
-              />
-              <div className={styles.timeTableWrapper} ref={elementRef}>
-                <FestivalStage timeTableInfo={boardData} />
-                <TimeTableBoard
-                  timeTableInfo={boardData}
-                  isEditMode={isEditTimeTableMode}
-                />
-              </div>
-              <TimeTableActions
-                isEditMode={isEditTimeTableMode}
-                onToggleEditMode={toggleEditTimeTableMode}
-                onDownload={downloadImage}
-              />
-            </div>
+            <TimeTableLoaded festivals={festivals} boardData={boardData} />
           ) : null,
       }}
       defaultComponent={() => null}
