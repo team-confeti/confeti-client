c736f90ad661256e18cec1a5c89252ea66af6919
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-06-02 20:46:11 +0900

    refactor: shared내 쿼리 관련 훅 정리

---
+++ apps/client/src/shared/hooks/queries/use-user-profile-query.ts
@@ -0,0 +1,12 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { USER_QUERY_OPTIONS } from '@shared/apis/user/user-queries';
+import { checkIsNotLoggedIn } from '@shared/utils/check-is-not-logged-in';
+
+export const useUserProfile = () => {
+  const { data } = useQuery({
+    ...USER_QUERY_OPTIONS.PROFILE(),
+    enabled: !checkIsNotLoggedIn(),
+  });
+  return { data };
+};
