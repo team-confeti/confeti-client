8252e2274fd7f12ec47c13477cdf8f93b011c16d
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-06-05 15:55:35 +0900

    refactor: 검색 로직에서 최근 검색 키워드 추가 기능 제거

--- apps/client/src/pages/search/hooks/use-search-logic.ts
+++ apps/client/src/pages/search/hooks/use-search-logic.ts
@@ -1,17 +1,13 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { useRecentSearch } from './use-recent-search';
-
 export const useSearchLogic = () => {
   const [barFocus, setBarFocus] = useState(false);
   const navigate = useNavigate();
-  // TODO: 커스텀 훅간 의존성 해결
-  const { addSearchKeyword } = useRecentSearch();
 
   const handleNavigateWithKeyword = (keyword: string) => {
     if (!keyword.trim()) return;
-    addSearchKeyword(keyword);
+
     navigate(`/search?q=${encodeURIComponent(keyword)}`);
     setBarFocus(false);
   };
