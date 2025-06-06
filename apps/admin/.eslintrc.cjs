709c962dbaa360205ac297dca3fe7fee7c7d6205
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-06-06 20:32:34 +0900

    init: admin 프로젝트 세팅

---
+++ apps/admin/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    '../../.eslintrc.cjs',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react/no-unused-state': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+  },
+};
---
+++ apps/admin/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Admin</div>;
+}
+
+export default App;
