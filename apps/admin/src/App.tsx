a75f52a149a1a53ecca2f9bdd43aafbdc6b43ee9
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-06-06 20:42:09 +0900

    feat: App 컴포넌트에 ThemeProvider 및 RouterProvider 추가

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,5 +1,14 @@
+import { RouterProvider } from 'react-router';
+
+import { ThemeProvider } from '@confeti/design-system';
+import router from '@shared/router/router';
+
 function App() {
-  return <div>Admin</div>;
+  return (
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
