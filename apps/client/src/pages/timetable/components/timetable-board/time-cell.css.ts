9b99a0fc968a7f4e026373e1d5ca680232acdedf
hanseo <yds06206@gmail.com>
2025-06-08 02:10:35 +0900

    style: 시간표 layout에 사용되는 rem단위 모두 px로 변경

--- apps/client/src/pages/timetable/components/timetable-board/time-cell.css.ts
+++ apps/client/src/pages/timetable/components/timetable-board/time-cell.css.ts
@@ -6,15 +6,15 @@ import { themeVars } from '@confeti/design-system/styles';
 export const timeList = style({
   display: 'flex',
   alignItems: 'center',
-  paddingBottom: '3rem',
+  paddingBottom: '46px',
   zIndex: themeVars.zIndex.timeTable.row,
 });
 
 export const timeP = recipe({
   base: {
     ...themeVars.fontStyles.body5_r_12,
-    padding: '0 0.4rem',
-    marginRight: '1.1rem',
+    padding: '0 4px',
+    marginRight: '11px',
     backgroundColor: themeVars.color.white,
   },
   variants: {
@@ -34,8 +34,9 @@ export const timeP = recipe({
 
 export const timeBar = recipe({
   base: {
-    height: '0.1rem',
-    width: 'calc(100% - 3rem)',
+    height: '1px',
+    width: 'calc(100% - 30px)',
+    margin: '0',
     color: themeVars.color.gray300,
     zIndex: themeVars.zIndex.timeTable.row,
   },
