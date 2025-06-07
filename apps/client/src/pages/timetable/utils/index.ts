27fa34c528a6ccb5deec373f7ef47ba2173278bc
hanseo <yds06206@gmail.com>
2025-06-08 02:49:45 +0900

    fix: 테이블 아이템 top, height 계산 로직 변경

--- apps/client/src/pages/timetable/utils/index.ts
+++ apps/client/src/pages/timetable/utils/index.ts
@@ -1,7 +1,4 @@
-import {
-  ONE_HOUR_TO_MINUTES,
-  TIME_SLOT_HEIGHT_5_MIN,
-} from '@pages/timetable/constants';
+import { ONE_HOUR_TO_MINUTES } from '@pages/timetable/constants';
 
 export const generateTableRow = (startTime: string) => {
   const startHour = Number(startTime);
@@ -15,13 +12,6 @@ export const parseTimeString = (timeString: string): string[] => {
 
   const [hour, min] = time.split(':');
   return [hour, min];
-};
-
-// TODO: 추후 수정 필요, 사용 안되고 있음
-export const calcPosition = (totalMin: number, minutesFromOpen: number) => {
-  const top = (minutesFromOpen / 5) * TIME_SLOT_HEIGHT_5_MIN;
-  const height = (totalMin / 5) * TIME_SLOT_HEIGHT_5_MIN;
-  return { top, height };
 };
 
 export const calcTotalMinutes = (
@@ -57,17 +47,4 @@ export const calcMinutesFromOpen = (
   const openTotalMin = openHourNum * ONE_HOUR_TO_MINUTES + openMinNum;
 
   return startTotalMin - openTotalMin;
-};
-
-export const calcTotalFestivalMinutes = (
-  startHour: string,
-  startMin: string,
-) => {
-  const startHourNum = Number(startHour);
-  const startMinNum = Number(startMin);
-
-  const hoursUntilEnd = 24 - startHourNum;
-  const totalFestivalMinutes = hoursUntilEnd * 60 - startMinNum;
-
-  return totalFestivalMinutes;
 };
