4a3fff2d25997d741974276247432803de301271
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-06-10 18:48:58 +0900

    feat: FestivalForm 내부 컴포넌트 분리

---
+++ apps/admin/src/pages/festival/components/date-field.tsx
@@ -0,0 +1,91 @@
+import { useFieldArray } from 'react-hook-form';
+import { Control, FieldErrors, UseFormRegister } from 'react-hook-form';
+import { z } from 'zod';
+
+import FormInput from '@shared/components/form/form-input';
+
+import { festivalSchema } from '../schemas/festival-schema';
+import ScheduleField from './schedule-field';
+
+import * as styles from './festival-form.css';
+
+interface Props {
+  dateIndex: number;
+  register: UseFormRegister<z.infer<typeof festivalSchema>>;
+  errors: FieldErrors<z.infer<typeof festivalSchema>>;
+  control: Control<z.infer<typeof festivalSchema>>;
+  onRemove: (index: number) => void;
+}
+
+function DateField({ dateIndex, register, errors, control, onRemove }: Props) {
+  const {
+    fields: scheduleFields,
+    append: appendSchedule,
+    remove: removeSchedule,
+  } = useFieldArray({
+    control,
+    name: `festivalDates.${dateIndex}.schedules`,
+  });
+
+  return (
+    <div className={styles.dateSection}>
+      <div className={styles.dateHeader}>
+        <div className={styles.subTitle}>날짜 {dateIndex + 1}</div>
+        <button
+          type="button"
+          className={styles.deleteButton}
+          onClick={() => onRemove(dateIndex)}
+        >
+          날짜 삭제
+        </button>
+      </div>
+
+      <div className={styles.inputContainer}>
+        <FormInput
+          {...register(`festivalDates.${dateIndex}.date`)}
+          type="date"
+          label="페스티벌 날짜"
+          placeholder="페스티벌 날짜를 입력해주세요."
+          error={errors.festivalDates?.[dateIndex]?.date?.message}
+        />
+        <FormInput
+          {...register(`festivalDates.${dateIndex}.ticketOpenTime`)}
+          type="text"
+          label="티켓 오픈 시간"
+          placeholder="티켓 오픈 시간을 입력해주세요."
+          error={errors.festivalDates?.[dateIndex]?.ticketOpenTime?.message}
+        />
+      </div>
+
+      {scheduleFields.map((schedule, scheduleIndex) => (
+        <ScheduleField
+          key={schedule.id}
+          scheduleIndex={scheduleIndex}
+          dateIndex={dateIndex}
+          register={register}
+          errors={errors}
+          control={control}
+          onRemove={removeSchedule}
+        />
+      ))}
+
+      <button
+        type="button"
+        onClick={() =>
+          appendSchedule({
+            stages: [
+              { startTime: '', endTime: '', artistIds: [{ value: '' }] },
+              { startTime: '', endTime: '', artistIds: [{ value: '' }] },
+              { startTime: '', endTime: '', artistIds: [{ value: '' }] },
+            ],
+          })
+        }
+        className={styles.addButton}
+      >
+        공연 시간 추가
+      </button>
+    </div>
+  );
+}
+
+export default DateField;
---
+++ apps/admin/src/pages/festival/components/stage-content-field.tsx
@@ -0,0 +1,132 @@
+import {
+  Control,
+  Controller,
+  FieldErrors,
+  useFieldArray,
+  UseFormRegister,
+} from 'react-hook-form';
+import { z } from 'zod';
+
+import FormInput from '@shared/components/form/form-input';
+
+import { festivalSchema } from '../schemas/festival-schema';
+
+import * as styles from './festival-form.css';
+
+export interface Props {
+  stageIndex: number;
+  scheduleIndex: number;
+  dateIndex: number;
+  register: UseFormRegister<z.infer<typeof festivalSchema>>;
+  errors: FieldErrors<z.infer<typeof festivalSchema>>;
+  control: Control<z.infer<typeof festivalSchema>>;
+  onRemoveSchedule: (index: number) => void;
+  isActive: boolean;
+}
+
+function StageContentField({
+  stageIndex,
+  scheduleIndex,
+  dateIndex,
+  register,
+  errors,
+  control,
+  onRemoveSchedule,
+  isActive,
+}: Props) {
+  const stageFields = useFieldArray({
+    control,
+    name: `festivalDates.${dateIndex}.schedules.${scheduleIndex}.stages.${stageIndex}.artistIds`,
+  });
+
+  const startTimeFieldName =
+    `festivalDates.${dateIndex}.schedules.${scheduleIndex}.stages.${stageIndex}.startTime` as const;
+  const endTimeFieldName =
+    `festivalDates.${dateIndex}.schedules.${scheduleIndex}.stages.${stageIndex}.endTime` as const;
+
+  return (
+    <div className={styles.stageContent}>
+      <div className={styles.stageHeader}>
+        <div className={styles.subTitle}>스테이지 {stageIndex + 1}</div>
+        {isActive && (
+          <button
+            type="button"
+            className={styles.deleteButton}
+            onClick={() => onRemoveSchedule(scheduleIndex)}
+          >
+            공연 시간 삭제
+          </button>
+        )}
+      </div>
+
+      <div className={styles.inputContainer}>
+        <Controller
+          control={control}
+          name={startTimeFieldName}
+          render={({ field }) => (
+            <FormInput
+              {...field}
+              type="text"
+              label={'공연 시작 시간'}
+              placeholder="공연 시작 시간을 입력해주세요."
+              error={
+                errors.festivalDates?.[dateIndex]?.schedules?.[scheduleIndex]
+                  ?.stages?.[stageIndex]?.startTime?.message
+              }
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name={endTimeFieldName}
+          render={({ field }) => (
+            <FormInput
+              {...field}
+              type="text"
+              label={'공연 종료 시간'}
+              placeholder="공연 종료 시간을 입력해주세요."
+              error={
+                errors.festivalDates?.[dateIndex]?.schedules?.[scheduleIndex]
+                  ?.stages?.[stageIndex]?.endTime?.message
+              }
+            />
+          )}
+        />
+      </div>
+
+      {stageFields.fields.map((artist, artistIndex) => (
+        <div key={artist.id} className={styles.artistInputContainer}>
+          <FormInput
+            {...register(
+              `festivalDates.${dateIndex}.schedules.${scheduleIndex}.stages.${stageIndex}.artistIds.${artistIndex}.value`,
+            )}
+            type="text"
+            label={`아티스트 ID ${artistIndex + 1}`}
+            placeholder="아티스트 ID를 입력해주세요."
+            error={
+              errors.festivalDates?.[dateIndex]?.schedules?.[scheduleIndex]
+                ?.stages?.[stageIndex]?.artistIds?.[artistIndex]?.value?.message
+            }
+          />
+          <button
+            type="button"
+            className={styles.deleteButton}
+            onClick={() => stageFields.remove(artistIndex)}
+          >
+            삭제
+          </button>
+        </div>
+      ))}
+
+      <button
+        type="button"
+        className={styles.addButton}
+        onClick={() => stageFields.append({ value: '' })}
+      >
+        아티스트 추가
+      </button>
+    </div>
+  );
+}
+
+export default StageContentField;
