60cd3251dde03d9826c6ecba61caf48307690b0b
bongtta <kce0419@naver.com>
2025-06-10 17:21:46 +0900

    feat: infiniteQueryOptions 사용

--- apps/client/src/pages/timetable/page/empty/empty-festival-section.tsx
+++ apps/client/src/pages/timetable/page/empty/empty-festival-section.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
 
 import { Button } from '@confeti/design-system';
 import { IcFestivalGray } from '@confeti/design-system/icons';
-import { getFestivalToAdd } from '@shared/apis/timetable/festival-timetable-queries';
+import { FESTIVAL_TIMETABLE_QUERY_OPTIONS } from '@shared/apis/timetable/festival-timetable-queries';
 import { routePath } from '@shared/router/path';
 
 import * as styles from './empty-festival-section.css';
@@ -11,13 +11,15 @@ import * as styles from './empty-festival-section.css';
 const EmptyFestivalSection = () => {
   const navigate = useNavigate();
 
-  const [isEmpty, setIsEmpty] = useState<boolean | null>(null);
-  useEffect(() => {
-    (async () => {
-      const response = await getFestivalToAdd();
-      setIsEmpty(response.festivals.length === 0);
-    })();
-  }, []);
+  const { data } = useSuspenseInfiniteQuery(
+    FESTIVAL_TIMETABLE_QUERY_OPTIONS.ADDABLE_FESTIVALS(),
+  );
+
+  const isEmpty = data.pages[0]?.festivals.length === 0;
+
+  const handleAddFestivalClick = () => {
+    navigate(isEmpty ? routePath.NO_UPCOMING_FESTIVAL : routePath.ADD_FESTIVAL);
+  };
 
   return (
     <section className={styles.container}>
@@ -32,12 +34,7 @@ const EmptyFestivalSection = () => {
       <Button
         className={styles.button}
         text="페스티벌 추가하기"
-        onClick={() => {
-          if (isEmpty === null) return;
-          navigate(
-            isEmpty ? routePath.NO_UPCOMING_FESTIVAL : routePath.ADD_FESTIVAL,
-          );
-        }}
+        onClick={handleAddFestivalClick}
       />
     </section>
   );
