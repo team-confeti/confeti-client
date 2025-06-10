fe465c939ce4b675d6203d95b4fa92beff60748e
daahyunk <ekgusdl0408@naver.com>
2025-06-11 04:27:49 +0900

    refactor: 미트볼 버튼 구현

--- apps/client/src/pages/timetable/components/festival-selector/festival-selector.css.ts
+++ apps/client/src/pages/timetable/components/festival-selector/festival-selector.css.ts
@@ -1,4 +1,7 @@
 import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
+
+import { themeVars } from '@confeti/design-system/styles';
 
 export const festivalSelectorWrapper = style({
   display: 'flex',
@@ -16,5 +19,26 @@ export const festivalButtonsWrapper = style({
 
 export const dropdownContainer = style({
   position: 'absolute',
-  right: '4.4rem',
+  right: '5rem',
+});
+
+export const meatballButton = recipe({
+  base: {
+    ...themeVars.display.flexJustifyAlignCenter,
+    width: '3rem',
+    height: '3rem',
+    borderRadius: '0.5rem',
+    border: `1px solid ${themeVars.color.gray200}`,
+    background: themeVars.color.white,
+    boxShadow: `0px 0px 6px 0px rgba(0, 0, 0, 0.15)`,
+    cursor: 'pointer',
+  },
+  variants: {
+    isOpen: {
+      true: {
+        border: `1px solid ${themeVars.color.confeti_lime3}`,
+      },
+      false: {},
+    },
+  },
 });
