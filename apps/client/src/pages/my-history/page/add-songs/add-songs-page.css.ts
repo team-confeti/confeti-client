f1ecbfc9758ea0bcfab94a1b4280473d1f86b5b8
Hanseo Kim <108131226+seueooo@users.noreply.github.com>
2025-06-12 16:18:06 +0900

    Refactor(client): 곡추가 곡검색 리팩토링 및 추가 플로우 반영 (#582)
    * feat: 연관 아티스트 선택 플로우 구현
    
    * feat: 아티스트 연관검색 뷰 구현
    
    * feat: 스타일 수정
    
    * refactor: 불필요한 코드 삭제
    
    * refactor: 타입에러 수정, 토큰 반영

--- apps/client/src/pages/my-history/page/add-songs/add-songs-page.css.ts
+++ apps/client/src/pages/my-history/page/add-songs/add-songs-page.css.ts
@@ -3,9 +3,8 @@ import { style } from '@vanilla-extract/css';
 import { themeVars } from '@confeti/design-system/styles';
 
 export const container = style({
-  height: 'calc(100dvh - 5rem)',
-  display: 'flex',
-  flexDirection: 'column',
+  flex: '1',
+  ...themeVars.display.flexColumn,
 });
 
 export const searchBarContainer = style({
@@ -30,4 +29,21 @@ export const buttonContainer = style({
 export const musicListContainer = style({
   padding: '1rem 2rem 1rem 2rem',
   cursor: 'pointer',
+});
+
+export const listContainer = style({
+  marginBottom: '1rem',
+});
+export const selectedComment = style({
+  ...themeVars.fontStyles.body3_m_14,
+  color: themeVars.color.gray600,
+  padding: '0.8rem 2rem',
+  marginBottom: '0.4rem',
+});
+
+export const suggestionContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  paddingTop: '2rem',
+  gap: '2rem',
 });
