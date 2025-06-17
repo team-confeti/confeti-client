a10ac29f357a994bb701d9d36e9f71c41d42e9fe
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-06-17 22:03:02 +0900

    refactor: menu.ts에서 tab.ts로 변경

---
+++ apps/client/src/pages/home/constants/tab.ts
@@ -0,0 +1,11 @@
+export enum TAB_MENU {
+  HOME = 0,
+  TIMETABLE = 1,
+  MY_HISTORY = 2,
+}
+
+export const enum HOME_CATEGORY_TAB {
+  TICKETING = '티켓 오픈',
+  SUGGEST_PERFORMANCE = '추천 공연',
+  PLAYLIST = '플레이리스트',
+}
--- apps/client/src/shared/components/navigation-tabs.tsx
+++ apps/client/src/shared/components/navigation-tabs.tsx
@@ -4,7 +4,7 @@ import { Navigation } from '@confeti/design-system';
 
 import { routePath } from '@shared/router/path';
 
-import { TAB_MENU } from '@pages/home/constants/menu';
+import { TAB_MENU } from '@pages/home/constants/tab';
 
 interface Props {
   defaultActiveTab: TAB_MENU;
