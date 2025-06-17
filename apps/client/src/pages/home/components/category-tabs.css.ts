06bf50b18ca9a498f76c96e4bd982e002889e306
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-06-17 22:15:50 +0900

    feat: 카테고리 탭 컴포넌트 리팩토링 및 스타일 추가

--- apps/client/src/pages/home/components/category-tabs.css.ts
+++ apps/client/src/pages/home/components/category-tabs.css.ts
@@ -1,5 +1,14 @@
 import { style } from '@vanilla-extract/css';
 
+import { themeVars } from '@confeti/design-system/styles';
+
+export const container = style({
+  position: 'sticky',
+  top: themeVars.size.height.header,
+  zIndex: themeVars.zIndex.header.content,
+  background: themeVars.color.white,
+});
+
 export const chipList = style({
   display: 'flex',
   gap: '0.8rem',
