6dbdf819c19aa190b7311e6fde9be6861e2216ba
gwagjiug <kwjo0228@naver.com>
2025-06-24 12:51:32 +0900

    feat(client): 온보딩 단계에서 이미지 소스 props로 변경 및 이미지 프리로드 훅 추가

--- apps/client/src/pages/timetable/page/onboading/timetable-onboard-page.tsx
+++ apps/client/src/pages/timetable/page/onboading/timetable-onboard-page.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useFunnel } from '@shared/hooks/use-funnel';
@@ -9,6 +10,23 @@ import FestivalCustomStep from '@pages/timetable/components/onboard/festival-cus
 import FestivalSelectStep from '@pages/timetable/components/onboard/festival-select-step';
 import IntroStep from '@pages/timetable/components/onboard/intro-step';
 import SaveTimetableStep from '@pages/timetable/components/onboard/save-timetable-step';
+import { useImagePreload } from '@pages/timetable/hooks/use-image-preload';
+
+import ImgOnboard1 from '/images/img_onboard_1.webp';
+import ImgOnboard2 from '/images/img_onboard_2.webp';
+import ImgOnboard3 from '/images/img_onboard_3.webp';
+import ImgOnboard4 from '/images/img_onboard_4.webp';
+import ImgOnboard5 from '/images/img_onboard_5.webp';
+import ImgOnboard6 from '/images/img_onboard_6.webp';
+
+const onboardImages = [
+  ImgOnboard1,
+  ImgOnboard2,
+  ImgOnboard3,
+  ImgOnboard4,
+  ImgOnboard5,
+  ImgOnboard6,
+];
 
 const TimetableOnboard = () => {
   const navigate = useNavigate();
@@ -28,6 +46,7 @@ const TimetableOnboard = () => {
       navigate(routePath.ADD_FESTIVAL);
     }
   };
+  useImagePreload(onboardImages);
 
   return (
     <Funnel>
@@ -35,6 +54,7 @@ const TimetableOnboard = () => {
         <IntroStep
           handleNextStep={handleNextStep(1)}
           handleNavigate={handleNavigate}
+          onboardImage={ImgOnboard1}
         />
       </Step>
       <Step name="2">
@@ -43,6 +63,7 @@ const TimetableOnboard = () => {
           handleNavigate={handleNavigate}
           totalIndex={TOTAL_INDEX}
           currentIndex={0}
+          onboardImage={ImgOnboard2}
         />
       </Step>
       <Step name="3">
@@ -51,6 +72,7 @@ const TimetableOnboard = () => {
           handleNavigate={handleNavigate}
           totalIndex={TOTAL_INDEX}
           currentIndex={1}
+          onboardImage={ImgOnboard3}
         />
       </Step>
       <Step name="4">
@@ -59,6 +81,7 @@ const TimetableOnboard = () => {
           handleNavigate={handleNavigate}
           totalIndex={TOTAL_INDEX}
           currentIndex={2}
+          onboardImage={ImgOnboard4}
         />
       </Step>
       <Step name="5">
@@ -67,6 +90,7 @@ const TimetableOnboard = () => {
           handleNavigate={handleNavigate}
           totalIndex={TOTAL_INDEX}
           currentIndex={3}
+          onboardImage={ImgOnboard5}
         />
       </Step>
       <Step name="6">
@@ -74,6 +98,7 @@ const TimetableOnboard = () => {
           handleNavigate={handleNavigate}
           totalIndex={TOTAL_INDEX}
           currentIndex={4}
+          onboardImage={ImgOnboard6}
         />
       </Step>
     </Funnel>
