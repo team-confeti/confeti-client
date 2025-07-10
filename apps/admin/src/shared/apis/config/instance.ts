c79c9cbd593236fe71c4d62fe239fdc905be353b
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-07-10 22:57:09 +0900

    refactor(admin): axios 의존성 제거

--- apps/admin/src/shared/apis/config/instance.ts
+++ apps/admin/src/shared/apis/config/instance.ts
@@ -1,31 +1,7 @@
-import axios from 'axios';
+import { createInstance } from '@confeti/core/http';
 
 import { ENV_CONFIG } from '@shared/constants/config';
 
-export const axiosInstance = axios.create({
-  baseURL: ENV_CONFIG.ADMIN_BASE_URL,
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-export function get<T>(...args: Parameters<typeof axiosInstance.get>) {
-  return axiosInstance.get<T>(...args).then((res) => res.data);
-}
-
-export function post<T>(...args: Parameters<typeof axiosInstance.post>) {
-  return axiosInstance.post<T>(...args).then((res) => res.data);
-}
-
-export function put<T>(...args: Parameters<typeof axiosInstance.put>) {
-  return axiosInstance.put<T>(...args).then((res) => res.data);
-}
-
-export function patch<T>(...args: Parameters<typeof axiosInstance.patch>) {
-  return axiosInstance.patch<T>(...args).then((res) => res.data);
-}
-
-export function del<T>(...args: Parameters<typeof axiosInstance.delete>) {
-  return axiosInstance.delete<T>(...args).then((res) => res.data);
-}
+export const { get, post, put, patch, del } = createInstance(
+  ENV_CONFIG.ADMIN_BASE_URL,
+);
