483cf731c6a25a49428737003c6b8aaffcfb5a02
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-07-10 23:22:54 +0900

    refactor: createInstance export 방식 수정

--- apps/client/src/shared/apis/config/instance.ts
+++ apps/client/src/shared/apis/config/instance.ts
@@ -4,11 +4,9 @@ import { ENV_CONFIG } from '@shared/constants/config';
 
 import { handleAPIError, handleCheckAndSetToken } from './interceptor';
 
-const { instance, get, post, put, patch, del } = createInstance(
+export const { instance, get, post, put, patch, del } = createInstance(
   ENV_CONFIG.BASE_URL,
 );
 
 instance.interceptors.request.use(handleCheckAndSetToken);
 instance.interceptors.response.use((res) => res, handleAPIError);
-
-export { del, get, instance, patch, post, put };
