e6cf255b9a6f7c78bfc3dee319c0539888e96c54
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-07-10 23:50:45 +0900

    refactor: TokenResponse 타입을 @confeti/core/auth로 이동하고 관련 코드 수정

--- apps/client/src/shared/apis/config/interceptor.ts
+++ apps/client/src/shared/apis/config/interceptor.ts
@@ -4,6 +4,7 @@ import {
   authTokenHandler,
   getAccessToken,
   getRefreshToken,
+  TokenResponse,
 } from '@confeti/core/auth';
 import {
   AxiosError,
@@ -17,7 +18,6 @@ import {
 import { END_POINT } from '@shared/constants/api';
 import { ENV_CONFIG } from '@shared/constants/config';
 import { routePath } from '@shared/router/path';
-import { TokenResponse } from '@shared/types/login-response';
 
 import { instance } from './instance';
 
