d263a8fc626b6aab446ac0550eabd9244cbf76cd
gwagjiug <kwjo0228@naver.com>
2025-07-24 18:32:54 +0900

    feat: onboarding 가드를 위한 컴포넌트 추가

---
+++ apps/client/src/shared/router/onboarding-guard.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Navigate, Outlet } from 'react-router-dom';
+
+import { ONBOARD_QUERY_OPTIONS } from '@shared/apis/onboard/queries';
+
+import { routePath } from './path';
+
+interface OnboardingGuardProps {
+  children?: ReactNode;
+}
+
+export const OnboardingGuard = ({ children }: OnboardingGuardProps) => {
+  const { data: onboardingStatus } = useQuery({
+    ...ONBOARD_QUERY_OPTIONS.STATUS(),
+  });
+
+  if (onboardingStatus?.isOnboarding === false) {
+    return <Navigate to={routePath.ROOT} replace />;
+  }
+
+  return children ? <>{children}</> : <Outlet />;
+};
+
+export const createOnboardingGuard = (Component: ReactNode) => (
+  <OnboardingGuard>{Component}</OnboardingGuard>
+);
