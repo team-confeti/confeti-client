6591930af63af3cdb33180c1c7b14cac20903c5a
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-09-24 15:29:52 +0900

    Refactor(client): 프로필 편집 페이지 개선 (#656)
    * refactor: 프로필 편집 저장 로직 수정 및 FormData 사용으로 변경
    
    * refactor: useReducer를 사용하여 프로필 편집 상태 관리 개선
    
    * feat: useEditProfile 커스텀 훅 제작

--- apps/client/src/pages/my/page/edit/edit-profile.tsx
+++ apps/client/src/pages/my/page/edit/edit-profile.tsx
@@ -1,4 +1,3 @@
-import React, { useRef, useState } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { Button, toast } from '@confeti/design-system';
@@ -12,6 +11,7 @@ import { useUserProfile } from '@shared/hooks/queries/use-user-profile-query';
 import EditNameInput from '@pages/my/components/edit/edit-name-input';
 import LinkedAccount from '@pages/my/components/edit/linked-account';
 import UserInfo from '@pages/my/components/profile/user-info';
+import { useEditProfile } from '@pages/my/hooks/use-edit-profile';
 
 import * as styles from './edit-profile.css';
 
@@ -21,77 +21,52 @@ const EditProfile = () => {
   const { mutate } = useMutation({
     ...USER_MUTATION_OPTIONS.PATCH_PROFILE(),
     onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: USER_QUERY_KEY.PROFILE(),
-      });
-      toast({
-        text: '성공적으로 저장되었어요!',
-        position: 'middleCenter',
-      });
+      queryClient.invalidateQueries({ queryKey: USER_QUERY_KEY.PROFILE() });
+      toast({ text: '성공적으로 저장되었어요!', position: 'middleCenter' });
     },
   });
 
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
-
-  const [name, setName] = useState('');
-  const [profileFile, setProfileFile] = useState<File | null>(null);
-  const [previewImgUrl, setPreviewImgUrl] = useState(
-    profileData?.profileUrl || '',
-  );
-  const [hasShownToast, setHasShownToast] = useState(false);
-
+  const {
+    state,
+    fileInputRef,
+    triggerFileInput,
+    handleInputChange,
+    handleFileChange,
+  } = useEditProfile(profileData?.profileUrl || '');
   if (!profileData) return null;
 
-  const isNameInvalid =
-    (name.length > 0 && name.length < 2) || name.length > 10;
-  const isImageChanged = !!profileFile;
-  const isButtonDisabled =
-    (name.length < 2 || isNameInvalid) && !isImageChanged;
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-    const shouldShowToast = showToastForNameLength(name, hasShownToast);
-    setHasShownToast(shouldShowToast);
-  };
-
-  const showToastForNameLength = (
-    name: string,
-    hasShownToast: boolean,
-  ): boolean => {
-    if (name.length > 9 && !hasShownToast) {
+  const showToastForNameLength = () => {
+    if (state.name.length > 9) {
       toast({
         text: '2~10자로 입력해 주세요',
         icon: <Icon name="toast-info" size="1.6rem" color="confeti_red" />,
         position: 'middleCenter',
       });
-      return true;
-    } else if (name.length <= 10 && hasShownToast) {
-      return false;
     }
-    return hasShownToast;
   };
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    handleInputChange(e.target.value);
+    showToastForNameLength();
+  };
+
+  const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
-    if (file) {
-      setProfileFile(file);
-      setPreviewImgUrl(URL.createObjectURL(file));
+    handleFileChange(file);
+  };
+
+  const handleSave = () => {
+    const newName = state.name || profileData.name;
+    const formData = new FormData();
+    formData.append('name', newName);
+    if (state.profileFile) {
+      formData.append('profileFile', state.profileFile);
     }
+    mutate(formData);
   };
 
-  const triggerFileInput = () => {
-    fileInputRef.current?.click();
-  };
-
-  const handleSave = async () => {
-    const newName = name || profileData.name;
-
-    const payload = profileFile
-      ? { name: newName, profileFile }
-      : { name: newName, profileUrl: profileData.profileUrl };
-
-    mutate(payload);
-  };
+  const isNameInvalid =
+    state.name.length > 0 && (state.name.length < 2 || state.name.length > 10);
 
   return (
     <>
@@ -100,24 +75,24 @@ const EditProfile = () => {
         <div className={styles.userInfo}>
           <UserInfo
             name={profileData.name}
-            profileUrl={previewImgUrl || profileData.profileUrl}
+            profileUrl={state.previewImgUrl || profileData.profileUrl}
             showArrow={false}
-            showEditBtn={true}
+            showEditBtn
             onEditImage={triggerFileInput}
-            disableClick={true}
+            disableClick
           />
           <input
             type="file"
             accept="image/*"
             ref={fileInputRef}
-            onChange={handleFileChange}
+            onChange={onChangeFile}
             style={{ display: 'none' }}
           />
         </div>
         <div className={styles.editProfileContent}>
           <EditNameInput
-            name={name}
-            onChange={handleInputChange}
+            name={state.name}
+            onChange={onChangeName}
             isInvalid={isNameInvalid}
           />
           <LinkedAccount />
@@ -126,7 +101,7 @@ const EditProfile = () => {
           <Button
             variant="add"
             text="저장하기"
-            disabled={isButtonDisabled}
+            disabled={isNameInvalid}
             onClick={handleSave}
           />
         </div>
