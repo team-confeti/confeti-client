7acfd2cfec8a239aca9248841a69efa23cdde7c7
gwagjiug <kwjo0228@naver.com>
2025-10-12 18:18:26 +0900

    fix: 아티스트 선택 컴포넌트에서 검색 바와 아바타 그리드 섹션의 렌더링 로직 개선

--- apps/client/src/pages/onboarding/components/artist-select.tsx
+++ apps/client/src/pages/onboarding/components/artist-select.tsx
@@ -67,50 +67,53 @@ const ArtistSelect = ({ setStep }: ArtistSelectProps) => {
     setStep(1);
   };
 
-  if (isFocused) {
-    return (
-      <ArtistSearch
-        onArtistSelect={(artistId) => handleArtistClick(artistId)}
-        handleSearchParams={handleSearchArtistSelect}
-      />
-    );
-  }
-
   return (
     <section className={styles.onboardingContentSection}>
-      <Description.Text
-        descriptionText={'선호하는 아티스트를\n모두 선택해주세요'}
-        fontSize={20}
-      />
-      <div className={styles.searchBarSection}>
-        <SearchBar
-          showBackButton={false}
-          placeholder="아티스트를 검색해주세요!"
-          onFocus={handleSearchBarFocus}
+      {isFocused ? (
+        <ArtistSearch
+          onArtistSelect={handleArtistClick}
+          handleSearchParams={handleSearchArtistSelect}
         />
-      </div>
-      <div className={styles.avatarGridSection}>
-        {artists.map((artist) => (
-          <div key={artist.artistId} className={styles.avatar}>
-            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
-              <Avatar
-                size="xl"
-                src={artist.profileUrl}
-                alt={`${artist.name} 이미지`}
-                onClick={() => handleArtistClick(artist.artistId)}
-              />
-            </motion.div>
-            <p className={styles.artistName}>{artist.name}</p>
+      ) : (
+        <>
+          <Description.Text
+            descriptionText={'선호하는 아티스트를\n모두 선택해주세요'}
+            fontSize={20}
+          />
+          <div className={styles.searchBarSection}>
+            <SearchBar
+              showBackButton={false}
+              placeholder="아티스트를 검색해주세요!"
+              onFocus={handleSearchBarFocus}
+            />
           </div>
-        ))}
-      </div>
-      <Button
-        text={'다음'}
-        variant={'add'}
-        onClick={handleNextClick}
-        disabled={selectedArtistIds.length < 1}
-        className={styles.button}
-      />
+          <div className={styles.avatarGridSection}>
+            {artists.map((artist) => (
+              <div key={artist.artistId} className={styles.avatar}>
+                <motion.div
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.8 }}
+                >
+                  <Avatar
+                    size="xl"
+                    src={artist.profileUrl}
+                    alt={`${artist.name} 이미지`}
+                    onClick={() => handleArtistClick(artist.artistId)}
+                  />
+                </motion.div>
+                <p className={styles.artistName}>{artist.name}</p>
+              </div>
+            ))}
+          </div>
+          <Button
+            text={'다음'}
+            variant={'add'}
+            onClick={handleNextClick}
+            disabled={selectedArtistIds.length < 1}
+            className={styles.button}
+          />
+        </>
+      )}
     </section>
   );
 };
