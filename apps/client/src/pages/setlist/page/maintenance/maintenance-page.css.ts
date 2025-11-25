cad3b402ea2599201b1d5ba5acadf10b853de5a8
m2na7 <minha3082@naver.com>
2025-11-26 01:59:06 +0900

    refactor: dvh로 모바일 높이 대응되도록 변경

--- apps/client/src/pages/setlist/page/maintenance/maintenance-page.css.ts
+++ apps/client/src/pages/setlist/page/maintenance/maintenance-page.css.ts
@@ -2,28 +2,11 @@ import { style } from '@vanilla-extract/css';
 
 import { themeVars } from '@confeti/design-system/styles';
 
-export const wrapper = style({
-  height: '100vh',
-  overflow: 'hidden',
-});
-
-export const navigationWrapper = style({
-  position: 'fixed',
-  top: 0,
-  left: 0,
-  right: 0,
-  zIndex: 10,
-  backgroundColor: themeVars.color.white,
-});
-
 export const container = style({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
+  ...themeVars.display.flexColumnAlignTextCenter,
   justifyContent: 'center',
-  height: 'calc(100vh - 48px)',
-  marginTop: '48px',
-  padding: '0 2rem',
+  minHeight: 'calc(100dvh - 98px)',
+  width: '100%',
 });
 
 export const image = style({
@@ -38,8 +21,7 @@ export const title = style({
 });
 
 export const description = style({
-  textAlign: 'center',
   ...themeVars.fontStyles.body4_m_13,
   color: themeVars.color.gray400,
-  lineHeight: '19.6px',
+  lineHeight: '20px',
 });
