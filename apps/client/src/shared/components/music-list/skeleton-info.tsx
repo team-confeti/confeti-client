4c831a2d2f4c6a166408b10f5fd757ef7736eb26
bongtta <kce0419@naver.com>
2025-12-05 04:49:37 +0900

    chore: width 수정

--- apps/client/src/shared/components/music-list/skeleton-info.tsx
+++ apps/client/src/shared/components/music-list/skeleton-info.tsx
@@ -11,7 +11,7 @@ const SkeletonInfo = () => {
           <Skeleton width={'10rem'} height={'14.2rem'} />
         </div>
         <div className={styles.textSection}>
-          <Skeleton width={'21rem'} height={'1.9rem'} />
+          <Skeleton width={'19rem'} height={'1.9rem'} />
           <div className={styles.buttonSection}>
             <p className={styles.buttonText}>공연 상세정보 확인하기</p>
             <Icon name="arrow-horizontal" size={12} color="white" />
