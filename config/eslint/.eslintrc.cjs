c7ce1e40e20cd9aa1061257194e34b80a37acacf
gwagjiug <kwjo0228@naver.com>
2026-02-01 14:15:38 +0900

    feat: es-toolkit 라이브러리 추가 및 ESLint 설정 업데이트

--- config/eslint/.eslintrc.cjs
+++ config/eslint/.eslintrc.cjs
@@ -1,5 +1,9 @@
 /* global module */
 module.exports = {
+  parser: '@typescript-eslint/parser',
+
+  plugins: ['@typescript-eslint', 'react', 'simple-import-sort'],
+
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -7,13 +11,13 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:prettier/recommended',
   ],
-  plugins: ['react', 'simple-import-sort'],
-  parser: '@typescript-eslint/parser',
+
   settings: {
     react: {
       version: 'detect',
     },
   },
+
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
@@ -23,6 +27,19 @@ module.exports = {
     ],
     'react/react-in-jsx-scope': 'off',
     'react/jsx-uses-react': 'off',
+    '@typescript-eslint/no-restricted-imports': [
+      'error',
+      {
+        patterns: [
+          {
+            group: ['es-toolkit', 'es-toolkit/*'],
+            message:
+              'es-toolkit은 직접 import하지 말고 shared/lib/es-toolkit/es.ts를 통해 사용해요.',
+          },
+        ],
+      },
+    ],
+
     'simple-import-sort/imports': [
       'error',
       {
@@ -40,4 +57,12 @@ module.exports = {
     ],
     'simple-import-sort/exports': 'error',
   },
+  overrides: [
+    {
+      files: ['**/shared/lib/es-toolkit/es.ts'],
+      rules: {
+        '@typescript-eslint/no-restricted-imports': 'off',
+      },
+    },
+  ],
 };
