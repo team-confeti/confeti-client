5605c3e7c0aa6ae9aaf704ac74ef2df209280bb3
곽지욱(クァク·ジウク) <99489686+gwagjiug@users.noreply.github.com>
2026-03-13 20:36:08 +0900

    Feat(Client): amplitude show, click 이벤트 로깅 환경 구축 (#782)

---
+++ apps/client/src/shared/analytics/logging.tsx
@@ -0,0 +1,34 @@
+import { Children, cloneElement, type ReactElement, useEffect } from 'react';
+
+import type { ClickEventPayload, ShowEventPayload } from './events/types';
+import { trackClickEvent, trackShowEvent } from './track';
+
+type LogClickEventProps = ClickEventPayload & {
+  children: ReactElement<{
+    onClick?: (...args: unknown[]) => void;
+  }>;
+};
+
+export const LogClickEvent = ({
+  children,
+  ...eventPayload
+}: LogClickEventProps) => {
+  const child = Children.only(children);
+
+  return cloneElement(child, {
+    onClick: (...args: unknown[]) => {
+      trackClickEvent(eventPayload);
+
+      return child.props.onClick?.(...args);
+    },
+  });
+};
+
+export const LogShowEvent = (props: ShowEventPayload) => {
+  useEffect(() => {
+    trackShowEvent(props);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return null;
+};
